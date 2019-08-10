541f5d11cdb74c3f1db774648897739bc92f0d8b
Eduardoqm <eduardobio2009@gmail.com>
2019-08-10 18:08:29 -0300

    Beautiful maps and some ajust in indexs

---
+++ Doutorado/PhD Geographic scripts/Beautiful_maps.R
@@ -0,0 +1,134 @@
+#Beautiful maps to show in presentations
+#Eduardo Q Marques 10/08/2019
+
+library(raster)
+library(rasterVis)
+library(rgdal)
+
+#Vectors
+area1 = readOGR(dsn = "C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/shapes",layer="Area1big")
+plots = readOGR(dsn = "C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/shapes",layer="Polygon_A_B_C")
+
+#NDVI
+a = stack(list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Tanguro Indices/NDVI", pattern = ".tif$", full.names=TRUE,recursive=TRUE))
+mapa = stack(a[[33]], a[[35]], a[[45]], a[[52]], a[[21]])
+names(mapa) = c('2004', '2005', '2008', '2011', '2019')
+
+#Transform vectors
+area1 = spTransform(area1, crs(mapa))
+plots = spTransform(plots, crs(mapa))
+
+#Crop rasters
+mapa = crop(mapa, area1)
+
+b = levelplot(mapa[[1]], margin = FALSE, par.settings = RdBuTheme, main = "NDVI 2004")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[2]], margin = FALSE, par.settings = RdBuTheme, main = "NDVI 2005")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[3]], margin = FALSE, par.settings = RdBuTheme, main = "NDVI 2008")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[4]], margin = FALSE, par.settings = RdBuTheme, main = "NDVI 2011")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[5]], margin = FALSE, par.settings = RdBuTheme, main = "NDVI 2019")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+
+
+#EVI
+a = stack(list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Tanguro Indices/EVI2", pattern = ".tif$", full.names=TRUE,recursive=TRUE))
+mapa = stack(a[[33]], a[[35]], a[[45]], a[[52]], a[[21]])
+names(mapa) = c('2004', '2005', '2008', '2011', '2019')
+
+#Crop rasters
+mapa = crop(mapa, area1)
+
+b = levelplot(mapa[[1]], margin = FALSE, par.settings = RdBuTheme, main = "EVI 2004")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[2]], margin = FALSE, par.settings = RdBuTheme, main = "EVI 2005")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[3]], margin = FALSE, par.settings = RdBuTheme, main = "EVI 2008")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[4]], margin = FALSE, par.settings = RdBuTheme, main = "EVI 2011")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[5]], margin = FALSE, par.settings = RdBuTheme, main = "EVI 2019")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+
+#NDMI
+a = stack(list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Tanguro Indices/NDMI", pattern = ".tif$", full.names=TRUE,recursive=TRUE))
+mapa = stack(a[[33]], a[[35]], a[[45]], a[[52]], a[[21]])
+names(mapa) = c('2004', '2005', '2008', '2011', '2019')
+
+#Crop rasters
+mapa = crop(mapa, area1)
+
+b = levelplot(mapa[[1]], margin = FALSE, par.settings = RdBuTheme, main = "NDMI 2004")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[2]], margin = FALSE, par.settings = RdBuTheme, main = "NDMI 2005")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[3]], margin = FALSE, par.settings = RdBuTheme, main = "NDMI 2008")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[4]], margin = FALSE, par.settings = RdBuTheme, main = "NDMI 2011")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[5]], margin = FALSE, par.settings = RdBuTheme, main = "NDMI 2019")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+
+#GRND
+a = stack(list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Tanguro Indices/GRND", pattern = ".tif$", full.names=TRUE,recursive=TRUE))
+mapa = stack(a[[33]], a[[35]], a[[45]], a[[52]], a[[21]])
+names(mapa) = c('2004', '2005', '2008', '2011', '2019')
+
+#Crop rasters
+mapa = crop(mapa, area1)
+
+b = levelplot(mapa[[1]], margin = FALSE, par.settings = RdBuTheme, main = "GRND 2004")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[2]], margin = FALSE, par.settings = RdBuTheme, main = "GRND 2005")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[3]], margin = FALSE, par.settings = RdBuTheme, main = "GRND 2008")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[4]], margin = FALSE, par.settings = RdBuTheme, main = "GRND 2011")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[5]], margin = FALSE, par.settings = RdBuTheme, main = "GRND 2019")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+
+#NDWI
+a = stack(list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Tanguro Indices/NDWI", pattern = ".tif$", full.names=TRUE,recursive=TRUE))
+mapa = stack(a[[33]], a[[35]], a[[45]], a[[52]], a[[21]])
+names(mapa) = c('2004', '2005', '2008', '2011', '2019')
+
+#Crop rasters
+mapa = crop(mapa, area1)
+
+b = levelplot(mapa[[1]], margin = FALSE, par.settings = RdBuTheme, main = "NDWI 2004")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[2]], margin = FALSE, par.settings = RdBuTheme, main = "NDWI 2005")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[3]], margin = FALSE, par.settings = RdBuTheme, main = "NDWI 2008")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[4]], margin = FALSE, par.settings = RdBuTheme, main = "NDWI 2011")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
+
+b = levelplot(mapa[[5]], margin = FALSE, par.settings = RdBuTheme, main = "NDWI 2019")
+b + layer(sp.lines(plots, lwd=2.0, col='black'))
--- Doutorado/PhD Geographic scripts/Calculate_Indices_landsat5-8.R
+++ Doutorado/PhD Geographic scripts/Calculate_Indices_landsat5-8.R
@@ -6,12 +6,12 @@ library(rasterVis)
 
 #TM-------------------------------------------------------------------------------------------------------------------
 #List of satellite bands (menos a banda 6)
-list1 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop5", pattern = "band1.tif$", full.names=TRUE,recursive=TRUE)
-list2 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop5", pattern = "band2.tif$", full.names=TRUE,recursive=TRUE)
-list3 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop5", pattern = "band3.tif$", full.names=TRUE,recursive=TRUE)
-list4 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop5", pattern = "band4.tif$", full.names=TRUE,recursive=TRUE)
-list5 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop5", pattern = "band5.tif$", full.names=TRUE,recursive=TRUE)
-list7 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop5", pattern = "band7.tif$", full.names=TRUE,recursive=TRUE)
+list1 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop5", pattern = "band1.tif$", full.names=TRUE,recursive=TRUE)
+list2 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop5", pattern = "band2.tif$", full.names=TRUE,recursive=TRUE)
+list3 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop5", pattern = "band3.tif$", full.names=TRUE,recursive=TRUE)
+list4 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop5", pattern = "band4.tif$", full.names=TRUE,recursive=TRUE)
+list5 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop5", pattern = "band5.tif$", full.names=TRUE,recursive=TRUE)
+list7 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop5", pattern = "band7.tif$", full.names=TRUE,recursive=TRUE)
 
 #Convert data into rasterbrick for faster processing
 b1 <- brick(stack(list1))
@@ -31,13 +31,13 @@ evi2 <- 2.5*((b4-b3)/(b4+2.4*b3+1))
 #Normalized Difference Vegetation Index (NDVI)
 ndvi <- (b4-b3)/(b4+b3)
 
-#Normalized Difference Glacier Index (NDGI) obs: Lenio (GRND)
-ndgi <- (b2-b3)/(b2+b3)
+#Green Red Normalized Difference (GRND)
+grnd <- (b2-b3)/(b2+b3)
 
 #Normalized Difference Moisture Index (NDMI) obs: Lenio (MSI)
 ndmi <- (b4-b5)/(b4+b5)
 
-#SWND???
+#SWND infrared index
 swnd <- (b5-b7)/(b5+b7)
 
 #Normalized Burned Ratio Index (NBRI)
@@ -45,6 +45,9 @@ nbri <- (b4-b7)/(b4+b7)
 
 #Normalized Pigment Chlorophyll Ratio Index (NPCRI)
 npcri <- (b3-b1)/(b3+b1)
+
+#Normalized Difference Water Index (NDWI)
+ndwi <- (b5-b2)/(b5+b2)
 
 #Salve NDVI rasters----------------------------------------------------------------------------------------------
 setwd("C:/Users/Eduardo Q Marques/Documents/EVI")
@@ -59,9 +62,9 @@ setwd("C:/Users/Eduardo Q Marques/Documents/NDVI")
 for (i in 1:length(ndvi)) {
   writeRaster(ndvi[[i]], names(b1[[i]]), format = "GTiff", bylayer = FALSE)
 }
-setwd("C:/Users/Eduardo Q Marques/Documents/NDGI")
-for (i in 1:length(ndgi)) {
-  writeRaster(ndgi[[i]], names(b1[[i]]), format = "GTiff", bylayer = FALSE)
+setwd("C:/Users/Eduardo Q Marques/Documents/GRND")
+for (i in 1:length(grnd)) {
+  writeRaster(grnd[[i]], names(b1[[i]]), format = "GTiff", bylayer = FALSE)
 }
 setwd("C:/Users/Eduardo Q Marques/Documents/NDMI")
 for (i in 1:length(ndmi)) {
@@ -79,16 +82,19 @@ setwd("C:/Users/Eduardo Q Marques/Documents/NPCRI")
 for (i in 1:length(npcri)) {
   writeRaster(npcri[[i]], names(b1[[i]]), format = "GTiff", bylayer = FALSE)
 }
-
+setwd("C:/Users/Eduardo Q Marques/Documents/NDWI")
+for (i in 1:length(ndwi)) {
+  writeRaster(ndwi[[i]], names(b1[[i]]), format = "GTiff", bylayer = FALSE)
+}
 
 #OLI------------------------------------------------------------------------------------------------------------------
 #List of satellite bands(banda 2 ate a banda 7 são equivalentes as bandas do landsat5)
-list2 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop8b", pattern = "band2.tif$", full.names=TRUE,recursive=TRUE)
-list3 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop8b", pattern = "band3.tif$", full.names=TRUE,recursive=TRUE)
-list4 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop8b", pattern = "band4.tif$", full.names=TRUE,recursive=TRUE)
-list5 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop8b", pattern = "band5.tif$", full.names=TRUE,recursive=TRUE)
-list6 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop8b", pattern = "band6.tif$", full.names=TRUE,recursive=TRUE)
-list7 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/landsatcrop8b", pattern = "band7.tif$", full.names=TRUE,recursive=TRUE)
+list2 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop8", pattern = "band2.tif$", full.names=TRUE,recursive=TRUE)
+list3 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop8", pattern = "band3.tif$", full.names=TRUE,recursive=TRUE)
+list4 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop8", pattern = "band4.tif$", full.names=TRUE,recursive=TRUE)
+list5 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop8", pattern = "band5.tif$", full.names=TRUE,recursive=TRUE)
+list6 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop8", pattern = "band6.tif$", full.names=TRUE,recursive=TRUE)
+list7 <- list.files(path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Landsat/landsatcrop8", pattern = "band7.tif$", full.names=TRUE,recursive=TRUE)
 
 #Convert data into rasterbrick for faster processing
 b2 <- brick(stack(list2))
@@ -109,7 +115,7 @@ evi2_b <- 2.5*((b5-b4)/(b5+2.4*b4+1))
 ndvi_b <- (b5-b4)/(b5+b4)
 
 #Normalized Difference Glacier Index (NDGI) obs: Lenio (GRND)
-ndgi_b <- (b3-b4)/(b3+b4)
+grnd_b <- (b3-b4)/(b3+b4)
 
 #Normalized Difference Moisture Index (NDMI) obs: Lenio (MSI)
 ndmi_b <- (b5-b6)/(b5+b6)
@@ -122,6 +128,9 @@ nbri_b <- (b5-b7)/(b5+b7)
 
 #Normalized Pigment Chlorophyll Ratio Index (NPCRI)
 npcri_b <- (b4-b2)/(b4+b2)
+
+#Normalized Difference Water Index (NDWI)
+ndwi_b <- (b6-b3)/(b6+b3)
 
 #Salve NDVI rasters-----------------------------------------------------------------------------------------
 setwd("C:/Users/Eduardo Q Marques/Documents/EVI")
@@ -136,9 +145,9 @@ setwd("C:/Users/Eduardo Q Marques/Documents/NDVI")
 for (i in 1:length(ndvi_b)) {
   writeRaster(ndvi_b[[i]], names(b2[[i]]), format = "GTiff", bylayer = FALSE)
 }
-setwd("C:/Users/Eduardo Q Marques/Documents/NDGI")
-for (i in 1:length(ndgi_b)) {
-  writeRaster(ndgi_b[[i]], names(b2[[i]]), format = "GTiff", bylayer = FALSE)
+setwd("C:/Users/Eduardo Q Marques/Documents/GRND")
+for (i in 1:length(grnd_b)) {
+  writeRaster(grnd_b[[i]], names(b2[[i]]), format = "GTiff", bylayer = FALSE)
 }
 setwd("C:/Users/Eduardo Q Marques/Documents/NDMI")
 for (i in 1:length(ndmi_b)) {
@@ -155,4 +164,8 @@ for (i in 1:length(nbri_b)) {
 setwd("C:/Users/Eduardo Q Marques/Documents/NPCRI")
 for (i in 1:length(npcri_b)) {
   writeRaster(npcri_b[[i]], names(b2[[i]]), format = "GTiff", bylayer = FALSE)
+}
+setwd("C:/Users/Eduardo Q Marques/Documents/NDWI")
+for (i in 1:length(ndwi_b)) {
+  writeRaster(ndwi_b[[i]], names(b2[[i]]), format = "GTiff", bylayer = FALSE)
 }
