f1bbe5b833205fa497075bde00e55dbdc33fa528
Eduardoqm <eduardobio2009@gmail.com>
2019-08-16 18:21:55 -0300

    Create Explore_Hyperion_data.R

---
+++ Doutorado/PhD Geographic scripts/Explore_Hyperion_data.R
@@ -0,0 +1,22 @@
+library(raster)
+library(rasterVis)
+
+setwd("C:/Users/Eduardo Q Marques/Documents/Eduardo_Hyper")
+a <- stack("Reflectance_22_July_2008")
+
+levelplot(a[[4]], margin = FALSE, par.settings = RdBuTheme)
+
+for (x in 1:152) {
+  print(a[[x]]@data@names)
+}
+
+p864 = a[[44]]
+
+p660 = a[[24]]
+
+ndvi = (p864-p660)/(p864+p660)
+ndvi2 = ndvi/10000
+
+levelplot(ndvi2, margin = FALSE, par.settings = RdBuTheme)
+
+#writeRaster(ndvi, filename="ndvi2008.tiff", overwrite=TRUE)
