9d68a48f739ec2a43542a0ff2f0d1d84a59de7f4
Eduardoqm <eduardobio2009@gmail.com>
2019-10-02 15:13:45 -0300

    Update Extract all Area-1 extention.R

--- Doutorado/PhD Geographic scripts/Dados rasterizados/Extract all Area-1 extention.R
+++ Doutorado/PhD Geographic scripts/Dados rasterizados/Extract all Area-1 extention.R
@@ -19,7 +19,7 @@ area1 <-readOGR(dsn = "C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/De
 area1 = spTransform(area1, crs(litt))
 
 #Liteira ===================================================================
-crt <- raster::extract(h04[[2]], area1[3,]); b3yr <- raster::extract(h04[[2]], area1[1,]); b1yr <- raster::extract(h04[[2]], area1[2,])
+b1yr <- raster::extract(litt[[1]], area1[3,]); crt <- raster::extract(litt[[1]], area1[1,]); b3yr <- raster::extract(litt[[1]], area1[2,])
 a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
 a = a %>% 
   mutate(parcela = "controle")
@@ -27,90 +27,94 @@ b = b %>%
   mutate(parcela = "b3yr")
 c = c %>% 
   mutate(parcela = "b1yr")
-ari1 = as.data.frame(rbind(a, b, c))
-ari1 <- ari1[,c(1,3)]
-colnames(ari1) = c("ari", "parcela")
-ari1 = ari1 %>% 
-  mutate(data = "2004")
-ari1_md = ari1 %>%
-  group_by(parcela) %>% 
-  summarise(ari = median(ari)) %>% 
-  mutate(data = "2004")
-
-crt <- raster::extract(h05[[2]], area1[3,]); b3yr <- raster::extract(h05[[2]], area1[1,]); b1yr <- raster::extract(h05[[2]], area1[2,])
-a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
-a = a %>% 
-  mutate(parcela = "controle")
-b = b %>% 
-  mutate(parcela = "b3yr")
-c = c %>% 
-  mutate(parcela = "b1yr")
-ari2 = as.data.frame(rbind(a, b, c))
-ari2 <- ari2[,c(1,3)]
-colnames(ari2) = c("ari", "parcela")
-ari2 = ari2 %>% 
-  mutate(data = "2005")
-ari2_md = ari2 %>%
-  group_by(parcela) %>% 
-  summarise(ari = median(ari)) %>% 
-  mutate(data = "2005")
-
-crt <- raster::extract(h06[[2]], area1[3,]); b3yr <- raster::extract(h06[[2]], area1[1,]); b1yr <- raster::extract(h06[[2]], area1[2,])
-a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
-a = a %>% 
-  mutate(parcela = "controle")
-b = b %>% 
-  mutate(parcela = "b3yr")
-c = c %>% 
-  mutate(parcela = "b1yr")
-ari3 = as.data.frame(rbind(a, b, c))
-ari3 <- ari3[,c(1,3)]
-colnames(ari3) = c("ari", "parcela")
-ari3 = ari3 %>% 
-  mutate(data = "2006")
-ari3_md = ari3 %>%
-  group_by(parcela) %>% 
-  summarise(ari = median(ari)) %>% 
-  mutate(data = "2006")
-
-crt <- raster::extract(h08[[2]], area1[3,]); b3yr <- raster::extract(h08[[2]], area1[1,]); b1yr <- raster::extract(h08[[2]], area1[2,])
-a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
-a = a %>% 
-  mutate(parcela = "controle")
-b = b %>% 
-  mutate(parcela = "b3yr")
-c = c %>% 
-  mutate(parcela = "b1yr")
-ari4 = as.data.frame(rbind(a, b, c))
-ari4 <- ari4[,c(1,3)]
-colnames(ari4) = c("ari", "parcela")
-ari4 = ari4 %>% 
-  mutate(data = "2008")
-ari4_md = ari4 %>%
-  group_by(parcela) %>% 
-  summarise(ari = median(ari)) %>% 
-  mutate(data = "2008")
-
-crt <- raster::extract(h10[[2]], area1[3,]); b3yr <- raster::extract(h10[[2]], area1[1,]); b1yr <- raster::extract(h10[[2]], area1[2,])
-a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
-a = a %>% 
-  mutate(parcela = "controle")
-b = b %>% 
-  mutate(parcela = "b3yr")
-c = c %>% 
-  mutate(parcela = "b1yr")
-ari5 = as.data.frame(rbind(a, b, c))
-ari5 <- ari5[,c(1,3)]
-colnames(ari5) = c("ari", "parcela")
-ari5 = ari5 %>% 
-  mutate(data = "2010")
-ari5_md = ari5 %>%
+litt1 = as.data.frame(rbind(a, b, c))
+litt1 <- litt1[,c(1,3)]
+colnames(litt1) = c("liteira", "parcela")
+litt1 = litt1 %>% 
   na.omit() %>% 
+  mutate(data = "2004")
+litt1_m = litt1 %>%
   group_by(parcela) %>% 
-  summarise(ari = median(ari)) %>% 
+  summarise(liteira = mean(liteira)) %>% 
+  mutate(data = "2004")
+
+b1yr <- raster::extract(litt[[2]], area1[3,]); crt <- raster::extract(litt[[2]], area1[1,]); b3yr <- raster::extract(litt[[2]], area1[2,])
+a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
+a = a %>% 
+  mutate(parcela = "controle")
+b = b %>% 
+  mutate(parcela = "b3yr")
+c = c %>% 
+  mutate(parcela = "b1yr")
+litt2 = as.data.frame(rbind(a, b, c))
+litt2 <- litt2[,c(1,3)]
+colnames(litt2) = c("liteira", "parcela")
+litt2 = litt2 %>% 
+  na.omit() %>% 
+  mutate(data = "2005")
+litt2_m = litt2 %>%
+  group_by(parcela) %>% 
+  summarise(liteira = mean(liteira)) %>% 
+  mutate(data = "2005")
+
+b1yr <- raster::extract(litt[[3]], area1[3,]); crt <- raster::extract(litt[[3]], area1[1,]); b3yr <- raster::extract(litt[[3]], area1[2,])
+a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
+a = a %>% 
+  mutate(parcela = "controle")
+b = b %>% 
+  mutate(parcela = "b3yr")
+c = c %>% 
+  mutate(parcela = "b1yr")
+litt3 = as.data.frame(rbind(a, b, c))
+litt3 <- litt3[,c(1,3)]
+colnames(litt3) = c("liteira", "parcela")
+litt3 = litt3 %>% 
+  na.omit() %>% 
+  mutate(data = "2006")
+litt3_m = litt3 %>%
+  group_by(parcela) %>% 
+  summarise(liteira = mean(liteira)) %>% 
+  mutate(data = "2006")
+
+b1yr <- raster::extract(litt[[4]], area1[3,]); crt <- raster::extract(litt[[4]], area1[1,]); b3yr <- raster::extract(litt[[4]], area1[2,])
+a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
+a = a %>% 
+  mutate(parcela = "controle")
+b = b %>% 
+  mutate(parcela = "b3yr")
+c = c %>% 
+  mutate(parcela = "b1yr")
+litt4 = as.data.frame(rbind(a, b, c))
+litt4 <- litt4[,c(1,3)]
+colnames(litt4) = c("liteira", "parcela")
+litt4 = litt4 %>% 
+  na.omit() %>% 
+  mutate(data = "2008")
+litt4_m = litt4 %>%
+  group_by(parcela) %>% 
+  summarise(liteira = mean(liteira)) %>% 
+  mutate(data = "2008")
+
+b1yr <- raster::extract(litt[[5]], area1[3,]); crt <- raster::extract(litt[[5]], area1[1,]); b3yr <- raster::extract(litt[[5]], area1[2,])
+a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
+a = a %>% 
+  mutate(parcela = "controle")
+b = b %>% 
+  mutate(parcela = "b3yr")
+c = c %>% 
+  mutate(parcela = "b1yr")
+litt5 = as.data.frame(rbind(a, b, c))
+litt5 <- litt5[,c(1,3)]
+colnames(litt5) = c("liteira", "parcela")
+litt5 = litt5 %>% 
+  na.omit() %>% 
+  mutate(data = "2010")
+litt5_m = litt5 %>%
+  group_by(parcela) %>% 
+  summarise(liteira = mean(liteira)) %>% 
   mutate(data = "2010")
 
-crt <- raster::extract(h11[[2]], area1[3,]); b3yr <- raster::extract(h11[[2]], area1[1,]); b1yr <- raster::extract(h11[[2]], area1[2,])
+b1yr <- raster::extract(litt[[6]], area1[3,]); crt <- raster::extract(litt[[6]], area1[1,]); b3yr <- raster::extract(litt[[6]], area1[2,])
 a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
 a = a %>% 
   mutate(parcela = "controle")
@@ -118,54 +122,55 @@ b = b %>%
   mutate(parcela = "b3yr")
 c = c %>% 
   mutate(parcela = "b1yr")
-ari6 = as.data.frame(rbind(a, b, c))
-ari6 <- ari6[,c(1,3)]
-colnames(ari6) = c("ari", "parcela")
-ari6 = ari6 %>% 
-  mutate(data = "2011")
-ari6_md = ari6 %>%
-  group_by(parcela) %>% 
-  summarise(ari = median(ari)) %>% 
-  mutate(data = "2011")
-
-crt <- raster::extract(h12[[2]], area1[3,]); b3yr <- raster::extract(h12[[2]], area1[1,]); b1yr <- raster::extract(h12[[2]], area1[2,])
-a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
-a = a %>% 
-  mutate(parcela = "controle")
-b = b %>% 
-  mutate(parcela = "b3yr")
-c = c %>% 
-  mutate(parcela = "b1yr")
-ari7 = as.data.frame(rbind(a, b, c))
-ari7 <- ari7[,c(1,3)]
-colnames(ari7) = c("ari", "parcela")
-ari7 = ari7 %>% 
-  mutate(data = "2012")
-ari7_md = ari7 %>%
-  group_by(parcela) %>% 
+litt6 = as.data.frame(rbind(a, b, c))
+litt6 <- litt6[,c(1,3)]
+colnames(litt6) = c("liteira", "parcela")
+litt6 = litt6 %>% 
   na.omit() %>% 
-  summarise(ari = median(ari)) %>% 
+  mutate(data = "2011")
+litt6_m = litt6 %>%
+  group_by(parcela) %>% 
+  summarise(liteira = mean(liteira)) %>% 
+  mutate(data = "2011")
+
+b1yr <- raster::extract(litt[[7]], area1[3,]); crt <- raster::extract(litt[[7]], area1[1,]); b3yr <- raster::extract(litt[[7]], area1[2,])
+a <- melt(crt); c <- melt(b1yr); b <- melt(b3yr)
+a = a %>% 
+  mutate(parcela = "controle")
+b = b %>% 
+  mutate(parcela = "b3yr")
+c = c %>% 
+  mutate(parcela = "b1yr")
+litt7 = as.data.frame(rbind(a, b, c))
+litt7 <- litt7[,c(1,3)]
+colnames(litt7) = c("liteira", "parcela")
+litt7 = litt7 %>% 
+  na.omit() %>% 
+  mutate(data = "2012")
+litt7_m = litt7 %>%
+  group_by(parcela) %>% 
+  summarise(liteira = mean(liteira)) %>% 
   mutate(data = "2012")
 
 
-ari = as.data.frame(rbind(ari1, ari2, ari3, ari4, ari5, ari6, ari7))
-ari_md = as.data.frame(rbind(ari1_md, ari2_md, ari3_md, ari4_md, ari5_md, ari6_md, ari7_md))
+litt = as.data.frame(rbind(litt1, litt2, litt3, litt4, litt5, litt6, litt7))
+litt_m = as.data.frame(rbind(litt1_m, litt2_m, litt3_m, litt4_m, litt5_m, litt6_m, litt7_m))
 
 #Boxplot
-ggplot(ari, aes(data,ari, col=parcela))+ 
+ggplot(litt, aes(data,litt, col=parcela))+ 
   geom_boxplot(outlier.alpha = 0.1)+
-  labs(fill= "Plot",x="Ano",y="ARI")+
+  labs(fill= "Plot",x="Ano",y="litt")+
   theme_minimal()
 
 #Medians
-ggplot(ari_md, aes(data,ari, col=parcela))+ 
+ggplot(litt_m, aes(data,litt, col=parcela))+ 
   geom_line(aes(group=parcela), size = 1)+
   geom_point()+
-  labs(fill= "Plot",x="Ano",y="ARI")+
+  labs(fill= "Plot",x="Ano",y="litt")+
   theme_minimal()
 
 #Extract difference
-diff = as.data.frame(cbind(ari1_md, ari2_md, ari3_md, ari4_md, ari5_md, ari6_md, ari7_md))
+diff = as.data.frame(cbind(litt1_m, litt2_m, litt3_m, litt4_m, litt5_m, litt6_m, litt7_m))
 diff = diff[,c(1,2,5,8,11,14,17,20)]
 colnames(diff) = c("parcela","2004","2005","2006","2008","2010","2011","2012")
 diff <- as.data.frame(t(diff))
@@ -188,6 +193,6 @@ gg$data = c(2004, 2005, 2006, 2008, 2010, 2011, 2012, 2004, 2005, 2006, 2008, 20
 ggplot(gg, aes(data,index, col=parcela))+ 
   geom_line(aes(group=parcela), size = 1)+
   geom_point()+
-  labs(fill= "Plot",x="Ano",y="Fogo - Controle (% Relative difference no ARI)")+
+  labs(fill= "Plot",x="Ano",y="Fogo - Controle (% Relative difference no litt)")+
   geom_hline(yintercept = 0, color = "black", linetype = "dashed")+
   theme_minimal()
