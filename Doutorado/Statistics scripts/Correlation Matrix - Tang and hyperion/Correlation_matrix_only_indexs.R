9a9cddfb1567c2fccd9134183e8b73de0b989fcd
Eduardoqm <eduardobio2009@gmail.com>
2019-11-04 12:05:08 -0300

    Update Correlation_matrix_only_indexs.R

--- Doutorado/Statistics scripts/Correlation Matrix - Tang and hyperion/Correlation_matrix_only_indexs.R
+++ Doutorado/Statistics scripts/Correlation Matrix - Tang and hyperion/Correlation_matrix_only_indexs.R
@@ -9,22 +9,16 @@ library(GGally)
 setwd('C:\\Users\\Eduardo Q Marques\\Documents\\My Jobs\\Doutorado\\Deposito\\Banco de Dados Tanguro\\Dados para analise cap1')
 
 hy = read.csv("Hyperion_indexs_median by plot.csv", sep = ",", header = TRUE)
-hy = hy[,-1]; hy = hy[,-1]; hy = hy[,-19]
+#hy = hy[,-1]; hy = hy[,-19]
+
+#All Indexs
+ggpairs(hy[,-1])
 
 
-ggpairs(hy)
+#Struture
+struc = hy[,c(2, 4, 10, 12, 18, 19, 21)]
 
-
-
-
-#Correlation bettewen all data
-ggcorr(hy, nbreaks = 10, label = T, low = "red3", high = "green3", 
-       label_round = 2, name = "Correlation Scale", label_alpha = T, hjust = 0.75) +
-  ggtitle(label = "Correlation Plot") +
-  theme(plot.title = element_text(hjust = 0.6))
-
-
-
+ggpairs(struc)
 
 
 
