1ae2eaa33962bcfccfb891c934cdd79189fcd324
Eduardoqm <eduardobio2009@gmail.com>
2019-11-05 11:39:21 -0300

    Update Correlation_matrix_hyperion_plot_data.R

--- Doutorado/Statistics scripts/Correlation Matrix - Tang and hyperion/Correlation_matrix_hyperion_plot_data.R
+++ Doutorado/Statistics scripts/Correlation Matrix - Tang and hyperion/Correlation_matrix_hyperion_plot_data.R
@@ -15,23 +15,23 @@ setwd('C:\\Users\\Eduardo Q Marques\\Documents\\My Jobs\\Doutorado\\Deposito\\Ba
 hy = read.csv("Hyperion_indexs_median by plot.csv", sep = ",", header = TRUE)
 
 #All Indexs =====================================
-ggpairs(hy[,-1])
+#ggpairs(hy[,-1])
 
 #Struture
-struc = hy[,c(2, 4, 10, 12, 18, 19)]
-ggpairs(struc)
+#struc = hy[,c(2, 4, 10, 12, 18, 19)]
+#ggpairs(struc)
 
 #Biochemistry
-bioc = hy[,c(2,3,5,6,9,11,14,15,17,20)]
-ggpairs(bioc)
+#bioc = hy[,c(2,3,5,6,9,11,14,15,17,20)]
+#ggpairs(bioc)
 
 #Physiology
-phy = hy[,c(2,13,16)]
-ggpairs(phy)
+#phy = hy[,c(2,13,16)]
+#ggpairs(phy)
 
 #Fire
-fir = hy[,c(2,7,8)]
-ggpairs(fir)
+#fir = hy[,c(2,7,8)]
+#ggpairs(fir)
 
 
 #Plot data organization
