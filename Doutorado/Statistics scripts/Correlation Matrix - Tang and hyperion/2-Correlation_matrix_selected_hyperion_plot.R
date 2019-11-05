c33c1400c4eab8700740652d5f97795895537e03
Eduardoqm <eduardobio2009@gmail.com>
2019-11-05 14:46:37 -0300

    Selected correlation matrix -> etapa 2

---
+++ Doutorado/Statistics scripts/Correlation Matrix - Tang and hyperion/2-Correlation_matrix_selected_hyperion_plot.R
@@ -0,0 +1,182 @@
+#######################################################
+# Correlaion - Vegetation Indexs                      #
+# Eduardo Q Marques  04/11/2019                       #
+#######################################################
+
+library(reshape2)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(GGally)
+library(stringr)
+
+setwd('C:\\Users\\Eduardo Q Marques\\Documents\\My Jobs\\Doutorado\\Deposito\\Banco de Dados Tanguro\\Dados para analise cap1')
+
+hy = read.csv("Hyperion_indexs_median by plot.csv", sep = ",", header = TRUE)
+
+#All Indexs =====================================
+#ggpairs(hy[,-1])
+
+#Struture
+#struc = hy[,c(2, 4, 10, 12, 18, 19)]
+#ggpairs(struc)
+
+#Biochemistry
+#bioc = hy[,c(2,3,5,6,9,11,14,15,17,20)]
+#ggpairs(bioc)
+
+#Physiology
+#phy = hy[,c(2,13,16)]
+#ggpairs(phy)
+
+#Fire
+#fir = hy[,c(2,7,8)]
+#ggpairs(fir)
+
+
+#Plot data organization
+#Biomass ====================================
+biomass = read.csv("Biomassa_Tang.csv", sep = ",", header = TRUE)
+biomass = biomass[,c(1:6)]
+colnames(biomass) = c('plot', '2004', '2008', '2010', '2011', '2012')
+biomass = melt(biomass)
+colnames(biomass) = c('plot', 'data', 'biomass')
+
+biomass = biomass %>% 
+  na.omit() %>% 
+  group_by(plot, data) %>% 
+  summarise(biomass = sum(biomass))%>%
+  ungroup()%>%
+  mutate(parcela=factor(plot,labels=c("controle","b3yr","b1yr")))
+  
+biomass = biomass[,-1]
+
+#LAI ====================================
+lai = read.csv("LAI_Area1_ABC_out2017.csv", sep = ",", header = TRUE)
+
+for (x in 1:10) {
+  lai$linhas[lai$linhas == x] <- "controle"
+}
+
+for (x in 11:20) {
+  lai$linhas[lai$linhas == x] <- "b3yr"
+}
+
+for (x in 21:31) {
+  lai$linhas[lai$linhas == x] <- "b1yr"
+}
+
+colnames(lai) = c("parcela", "lai", "data")
+
+lai = lai %>% 
+  group_by(parcela, data) %>% 
+  summarise(lai = median(lai)) %>% 
+  filter(data == 2005:2011)
+
+#Fuel ==================================
+fuel = read.csv("Combustivel_Brown_Tang.csv", sep = ",", header = TRUE)
+
+fuel = fuel %>% 
+  na.omit() %>% 
+  mutate(fuel = NI06 + NI25 + NI76) 
+
+fuel = fuel[,c(1,2,7)]
+
+# extract numbers only
+fuel$ponto <- as.numeric(str_extract(fuel$ponto, "[0-9]+"))
+fuel = na.omit(fuel)
+colnames(fuel) = c("parcela", "data", "fuel")
+
+for (x in 1:10) {
+  fuel$parcela[fuel$parcela == x] <- "controle"
+}
+
+for (x in 11:20) {
+  fuel$parcela[fuel$parcela == x] <- "b3yr"
+}
+
+for (x in 21:31) {
+  fuel$parcela[fuel$parcela == x] <- "b1yr"
+}
+
+fuel = fuel %>% 
+  group_by(parcela, data) %>% 
+  summarise(fuel = median(fuel))
+
+#Join everything =========================
+#Struture
+struc = hy[,c(2, 4, 10, 12, 18, 19, 21)]
+
+struc = struc %>% 
+  unite(col = "id", c("parcela", "data"), sep = '_')
+
+biomass = biomass %>% 
+  unite(col = "id", c("parcela", "data"), sep = '_')
+
+lai = lai %>% 
+  unite(col = "id", c("parcela", "data"), sep = '_')
+
+fuel = fuel %>% 
+  unite(col = "id", c("parcela", "data"), sep = '_')
+
+
+struc = full_join(struc, biomass, by="id")
+struc = full_join(struc, lai, by="id")
+struc = full_join(struc, fuel, by="id")
+
+struc = struc %>% 
+  separate(col = "id", c("parcela", "data"), sep = '_')
+
+ggpairs(struc[,-2])
+
+#Biochemistry
+bioc = hy[,c(2,3,5,6,9,11,14,15,17,20,21)]
+
+bioc = bioc %>% 
+  unite(col = "id", c("parcela", "data"), sep = '_')
+
+bioc = full_join(bioc, biomass, by="id")
+bioc = full_join(bioc, lai, by="id")
+bioc = full_join(bioc, fuel, by="id")
+
+bioc = bioc %>% 
+  separate(col = "id", c("parcela", "data"), sep = '_')
+
+ggpairs(bioc[,-2])
+
+#Physiology
+phy = hy[,c(2,13,16,21)]
+
+phy = phy %>% 
+  unite(col = "id", c("parcela", "data"), sep = '_')
+
+phy = full_join(phy, biomass, by="id")
+phy = full_join(phy, lai, by="id")
+phy = full_join(phy, fuel, by="id")
+
+phy = phy %>% 
+  separate(col = "id", c("parcela", "data"), sep = '_')
+
+ggpairs(phy[,-2])
+
+#Fire
+fir = hy[,c(2,7,8,21)]
+
+fir = fir %>% 
+  unite(col = "id", c("parcela", "data"), sep = '_')
+
+fir = full_join(fir, biomass, by="id")
+fir = full_join(fir, lai, by="id")
+fir = full_join(fir, fuel, by="id")
+
+fir = fir %>% 
+  separate(col = "id", c("parcela", "data"), sep = '_')
+
+ggpairs(fir[,-2])
+
+
+
+
+
+
+
