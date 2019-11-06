e485015d582102bd7f7479932aaebd6192da4ee5
Eduardoqm <eduardobio2009@gmail.com>
2019-11-06 10:03:27 -0300

    NDII solved

--- Doutorado/Statistics scripts/Correlation Matrix - Tang and hyperion/2-Correlation_matrix_selected_hyperion_plot.R
+++ Doutorado/Statistics scripts/Correlation Matrix - Tang and hyperion/2-Correlation_matrix_selected_hyperion_plot.R
@@ -14,26 +14,6 @@ setwd('C:\\Users\\Eduardo Q Marques\\Documents\\My Jobs\\Doutorado\\Deposito\\Ba
 
 hy = read.csv("Hyperion_indexs_median by plot.csv", sep = ",", header = TRUE)
 
-#All Indexs =====================================
-#ggpairs(hy[,-1])
-
-#Struture
-#struc = hy[,c(2, 4, 10, 12, 18, 19)]
-#ggpairs(struc)
-
-#Biochemistry
-#bioc = hy[,c(2,3,5,6,9,11,14,15,17,20)]
-#ggpairs(bioc)
-
-#Physiology
-#phy = hy[,c(2,13,16)]
-#ggpairs(phy)
-
-#Fire
-#fir = hy[,c(2,7,8)]
-#ggpairs(fir)
-
-
 #Plot data organization
 #Biomass ====================================
 biomass = read.csv("Biomassa_Tang.csv", sep = ",", header = TRUE)
