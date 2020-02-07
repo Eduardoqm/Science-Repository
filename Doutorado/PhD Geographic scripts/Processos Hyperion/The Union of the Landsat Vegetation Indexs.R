6c1681e0367d3b159f2ad4228ecb377816c1b18c
Eduardoqm <eduardobio2009@gmail.com>
2020-02-07 16:21:43 -0300

    Data - Dashboard

---
+++ Doutorado/PhD Geographic scripts/Processos Hyperion/The Union of the Landsat Vegetation Indexs.R
@@ -0,0 +1,7 @@
+###############################################
+# THE UNION OF THE LANDSAT VEGETATION INDEXS  #
+#          => A Temporal scale <==            #
+#            => CORE and EDGe <=              #
+# By: Eduardo Q Marques   06-02-2020          #
+###############################################
+
---
+++ Doutorado/Processos de bancos de dados/Comparison data - Sattelite and Area1 plot.R
@@ -0,0 +1,3 @@
+#Plots to comparation Insdex data and Area1 data
+
+#Eduardo Q Marques 06-02-2020
