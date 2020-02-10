145e1d4be8b9730c80cb644a458501fc4b089730
Eduardoqm <eduardobio2009@gmail.com>
2020-02-10 15:26:46 -0300

    Update Comparison data - Sattelite and Area1 plot.R

--- Doutorado/Processos de bancos de dados/Comparison data - Sattelite and Area1 plot.R
+++ Doutorado/Processos de bancos de dados/Comparison data - Sattelite and Area1 plot.R
@@ -12,12 +12,12 @@ setwd('C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de 
 
 landsat = read.csv("Landast_indexs_median by plot.csv")
 landsat = landsat %>% 
-  unite(col = "id", c("parcela", "date"), sep = '_')
+  unite(col = "id", c("parcela", "date", "dist"), sep = '_')
 
 
 hyper = read.csv("Hyperion_indexs_median by plot_2.csv")
 hyper = hyper %>% 
-  unite(col = "id", c("parcela", "date"), sep = '_')
+  unite(col = "id", c("parcela", "date", "dist"), sep = '_')
 
 
 satellite = full_join(landsat, hyper, by = "id")
