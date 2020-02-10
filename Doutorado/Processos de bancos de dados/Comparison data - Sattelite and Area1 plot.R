ca20c87497a887357405e17dd0dc84fb94c9734c
Eduardoqm <eduardobio2009@gmail.com>
2020-02-10 15:48:46 -0300

    Update Comparison data - Sattelite and Area1 plot.R

--- Doutorado/Processos de bancos de dados/Comparison data - Sattelite and Area1 plot.R
+++ Doutorado/Processos de bancos de dados/Comparison data - Sattelite and Area1 plot.R
@@ -10,14 +10,41 @@ library(viridis)
 #Data bank
 setwd('C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Dados para analise cap1')
 
+#Lansat
 landsat = read.csv("Landast_indexs_median by plot.csv")
 landsat = landsat %>% 
   unite(col = "id", c("parcela", "date", "dist"), sep = '_')
 
-
+#Hyperion
 hyper = read.csv("Hyperion_indexs_median by plot_2.csv")
 hyper = hyper %>% 
   unite(col = "id", c("parcela", "date", "dist"), sep = '_')
 
-
+#All satellite data
 satellite = full_join(landsat, hyper, by = "id")
+satellite = satellite %>%
+  separate(col = "id", c("parcela", "date", "dist"), sep = '_')
+
+#Process to plot
+sat = melt(satellite)
+
+sat_edge = sat %>%
+  filter(dist == "borda")
+
+sat_core = sat %>%
+  filter(dist == "nucleo")
+
+#Plot data
+ggplot(sat_edge, aes(date, value, color = variable))+
+  geom_line(aes(group=parcela), size = 1)
+
+
+
+
+
+
+
+
+
+
+
