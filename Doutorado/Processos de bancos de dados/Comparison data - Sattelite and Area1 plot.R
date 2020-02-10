2ed47b8ed51d9b834cff184e9a1d245dc2e05f22
Eduardoqm <eduardobio2009@gmail.com>
2020-02-10 16:55:46 -0300

    Update Comparison data - Sattelite and Area1 plot.R

--- Doutorado/Processos de bancos de dados/Comparison data - Sattelite and Area1 plot.R
+++ Doutorado/Processos de bancos de dados/Comparison data - Sattelite and Area1 plot.R
@@ -12,6 +12,9 @@ setwd('C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de 
 
 #Lansat
 landsat = read.csv("Landast_indexs_median by plot.csv")
+
+landsat = landsat[,c(-6)] #NDWI have so high values in comparison with other indexs
+
 landsat = landsat %>% 
   unite(col = "id", c("parcela", "date", "dist"), sep = '_')
 
@@ -19,6 +22,7 @@ landsat = landsat %>%
 hyper = read.csv("Hyperion_indexs_median by plot_2.csv")
 
 #hyper = hyper[,c(-12)] #PSSR have so high values in comparison with other indexs
+hyper = hyper[,c(-9)] #NDWI have so high values in comparison with other indexs
 
 hyper = hyper %>% 
   unite(col = "id", c("parcela", "date", "dist"), sep = '_')
@@ -28,7 +32,7 @@ satellite = full_join(landsat, hyper, by = "id")
 satellite = satellite %>%
   separate(col = "id", c("parcela", "date", "dist"), sep = '_')
 
-#Process to plot
+#Process to plot ==================
 sat = melt(satellite)
 
 sat_edge_crt = sat %>%
@@ -50,42 +54,31 @@ sat_core_b3y = sat %>%
 sat_core_b1y = sat %>%
   filter(dist == "nucleo", parcela == "b1yr")
 
-#Plot data
-ggplot(sat_edge_crt, aes(date, value, color = variable))+
-  geom_line(aes(group=variable), size = 1)+
-  geom_point()
-
-  
-ggplot(sat_edge_b3y, aes(date, value, color = variable))+
-  geom_line(aes(group=variable), size = 1)
-
-ggplot(sat_edge_b1y, aes(date, value, color = variable))+
-  geom_line(aes(group=variable), size = 1)+
-  geom_point()
-
-
 #Calculate difference
 diff_edge = sat_edge_crt[,c(1,2,3,4)]
+
 diff_edge$b3yr = ((sat_edge_b3y$value-sat_edge_crt$value)*100)/sat_edge_crt$value
 diff_edge$b1yr = ((sat_edge_b1y$value-sat_edge_crt$value)*100)/sat_edge_crt$value
 
+diff_edge = diff_edge[,c(-1)]
+
 
 diff_core = sat_core_crt[,c(1,2,3,4)]
+
 diff_core$b3yr = ((sat_core_b3y$value-sat_core_crt$value)*100)/sat_core_crt$value
 diff_core$b1yr = ((sat_core_b1y$value-sat_core_crt$value)*100)/sat_core_crt$value
 
+diff_core = diff_core[,c(-1)]
 
 
+#Plot data ===============
+gg = melt(diff_edge)
+colnames(gg) = c("date","dist", "index", "parcela", "value")
 
-gg = melt(diff)
-colnames(gg) = c("parcela","index")
-gg$data = c(2004, 2005, 2006, 2008, 2010, 2011, 2012, 2004, 2005, 2006, 2008, 2010, 2011, 2012)
-
-
-ggplot(gg, aes(data,index, col=parcela))+ 
-  geom_line(aes(group=parcela), size = 1)+
+ggplot(gg, aes(date,value, col=index))+ 
+  geom_line(aes(group=index), size = 1)+
   geom_point()+
-  labs(fill= "Plot",x="Ano",y="Fogo - Controle (% Relative difference no WBI)")+
+  labs(fill= "Plot",x="Ano",y="Fogo - Controle (% Relative difference)")+
   geom_hline(yintercept = 0, color = "black", linetype = "dashed")+
   theme_minimal()
 
