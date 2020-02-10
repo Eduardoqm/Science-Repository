c1bdba71a55e7ce6328f82f0f5c3888994e76c7d
Eduardoqm <eduardobio2009@gmail.com>
2020-02-10 16:46:01 -0300

    Update Comparison data - Sattelite and Area1 plot.R

--- Doutorado/Processos de bancos de dados/Comparison data - Sattelite and Area1 plot.R
+++ Doutorado/Processos de bancos de dados/Comparison data - Sattelite and Area1 plot.R
@@ -18,7 +18,7 @@ landsat = landsat %>%
 #Hyperion
 hyper = read.csv("Hyperion_indexs_median by plot_2.csv")
 
-hyper = hyper[,c(-12)]
+#hyper = hyper[,c(-12)] #PSSR have so high values in comparison with other indexs
 
 hyper = hyper %>% 
   unite(col = "id", c("parcela", "date", "dist"), sep = '_')
@@ -54,8 +54,40 @@ sat_core_b1y = sat %>%
 ggplot(sat_edge_crt, aes(date, value, color = variable))+
   geom_line(aes(group=variable), size = 1)+
   geom_point()
-  
 
+  
+ggplot(sat_edge_b3y, aes(date, value, color = variable))+
+  geom_line(aes(group=variable), size = 1)
+
+ggplot(sat_edge_b1y, aes(date, value, color = variable))+
+  geom_line(aes(group=variable), size = 1)+
+  geom_point()
+
+
+#Calculate difference
+diff_edge = sat_edge_crt[,c(1,2,3,4)]
+diff_edge$b3yr = ((sat_edge_b3y$value-sat_edge_crt$value)*100)/sat_edge_crt$value
+diff_edge$b1yr = ((sat_edge_b1y$value-sat_edge_crt$value)*100)/sat_edge_crt$value
+
+
+diff_core = sat_core_crt[,c(1,2,3,4)]
+diff_core$b3yr = ((sat_core_b3y$value-sat_core_crt$value)*100)/sat_core_crt$value
+diff_core$b1yr = ((sat_core_b1y$value-sat_core_crt$value)*100)/sat_core_crt$value
+
+
+
+
+gg = melt(diff)
+colnames(gg) = c("parcela","index")
+gg$data = c(2004, 2005, 2006, 2008, 2010, 2011, 2012, 2004, 2005, 2006, 2008, 2010, 2011, 2012)
+
+
+ggplot(gg, aes(data,index, col=parcela))+ 
+  geom_line(aes(group=parcela), size = 1)+
+  geom_point()+
+  labs(fill= "Plot",x="Ano",y="Fogo - Controle (% Relative difference no WBI)")+
+  geom_hline(yintercept = 0, color = "black", linetype = "dashed")+
+  theme_minimal()
 
 
 
