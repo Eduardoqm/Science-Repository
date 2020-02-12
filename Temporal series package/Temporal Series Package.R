912b47755c5fe31de37906b9cd492418c78d7e4a
Eduardoqm <eduardobio2009@gmail.com>
2020-02-12 17:49:04 -0300

    Point function

--- Temporal series package/Temporal Series Package.R
+++ Temporal series package/Temporal Series Package.R
@@ -16,6 +16,22 @@ library(tidyr)
 library(viridis)
 
 #Functions =============================================================================
+#Extract point temporal data (x is raster, y is point file)
+time_point = function(x,y){
+  ext <- raster::extract(x, y)
+  a <- melt(ext)
+  a_md = a %>%
+    group_by(date, parcela) %>% 
+    summarise(index = median(index))
+}
+
+
+
+
+
+
+
+
 #To calculate median per plot
 data_time_median = function(x,y){
   ext <- raster::extract(x, y)
