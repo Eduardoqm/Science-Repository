2e626727c7dd6bcb26304a742174d7df77119bca
Eduardoqm <eduardobio2009@gmail.com>
2020-02-14 15:38:30 -0300

    Integration

--- Doutorado/Analysis/Analises exploratorias/Explore_Hyperion_and_Area1_data.R
+++ Doutorado/Analysis/Analises exploratorias/Explore_Hyperion_and_Area1_data.R
@@ -13,7 +13,7 @@ library(ggplot2)
 setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Dados para analise cap1")
 
 hy = read.csv("Hyperion_indexs_median by plot.csv", sep = ",", header = TRUE)
-hyper = hyper[,c(-9)] #NDWI have so high values in comparison with other indexs
+hy = hy[,c(-9)] #NDWI have so high values in comparison with other indexs
 
 biomass = read.csv("Biomass_tang.csv", sep = ",", header = TRUE)
 
@@ -31,6 +31,8 @@ litt  = read.csv("Liteira_tang.csv", sep = ",", header = TRUE)
 biomass = biomass %>% 
   filter(data <=2012)
 
+colnames(biomass) = c("id", "biomass")
+
 #LAI
 lai = lai %>% 
   filter(data <=2012)
@@ -38,6 +40,8 @@ lai = lai %>%
 #Litterfall
 litt = litt %>% 
   filter(data <=2012)
+
+colnames(litt) = c("id", "litter")
 
 #Join Data
 hy = hy %>% 
@@ -61,6 +65,7 @@ df = full_join(df, litt, by="id")
 
 df = df %>% 
   separate(col = "id", c("parcela", "data", "dist"), sep = '_')
+
 
 #write.table(df, "Integration_Area1_data_edge_core.csv", sep = ",")
 #Correlation Matrix ======================================================================
@@ -121,8 +126,92 @@ df_diff_core$b1yr = ((df_core_b1yr$value-df_core_crt$value)*100)/df_core_crt$val
 
 df_diff_core = df_diff_core[,c(-1)]
 
+#DF Plots ==========================================================================
+#DF edge ================
+gg_edge = melt(df_diff_edge)
+colnames(gg_edge) = c("date","dist", "index", "parcela", "value")
+gg1 = gg_edge %>% filter(parcela == 'b1yr')
+gg2 = gg_edge %>% filter(parcela == 'b3yr')
+
+ggplot(gg1, aes(date,value, col=index))+ 
+  geom_line(aes(group=index), size = 1)+
+  #geom_point()+
+  labs(fill= "Plot",x="Year",y="B1yr - Control (% Relative difference Edge)")+
+  geom_hline(yintercept = 0, color = "gray", linetype = "dashed")+
+  
+  annotate("text", x = c(3.5, 5.5), y = 110, label = "Dry")+
+  
+  
+  annotate("text", x = 3, y = -123, label = "Fire experiment", colour = "red")+
+  annotate("rect", xmin = 1, xmax = 6, ymin = -129, ymax = -135, alpha = 0.9, fill = "red")+
+  annotate("text", x = c(1.5, 3.7, 5.5), y = -130, label = "xx", colour = "yellow")+
+  
+  annotate("text", x = 6.5, y = -123, label = "Restoration", colour = "darkblue")+
+  annotate("rect", xmin = 6, xmax = 7, ymin = -129, ymax = -135, alpha = 0.9, fill = "darkblue")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90))
 
 
+ggplot(gg2, aes(date,value, col=index))+ 
+  geom_line(aes(group=index), size = 1)+
+  #geom_point()+
+  labs(fill= "Plot",x="Year",y="B3yr - Control (% Relative difference Edge)")+
+  geom_hline(yintercept = 0, color = "gray", linetype = "dashed")+
+  
+  annotate("text", x = c(3.5, 5.5), y = 110, label = "Dry")+
+  
+  
+  annotate("text", x = 3, y = -123, label = "Fire experiment", colour = "red")+
+  annotate("rect", xmin = 1, xmax = 6, ymin = -129, ymax = -135, alpha = 0.9, fill = "red")+
+  annotate("text", x = c(1.5, 3.7, 5.5), y = -130, label = "xx", colour = "yellow")+
+  
+  annotate("text", x = 6.5, y = -123, label = "Restoration", colour = "darkblue")+
+  annotate("rect", xmin = 6, xmax = 7, ymin = -129, ymax = -135, alpha = 0.9, fill = "darkblue")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90))
+
+
+#DF core ================
+gg_core = melt(df_diff_core)
+colnames(gg_core) = c("date","dist", "index", "parcela", "value")
+gg3 = gg_core %>% filter(parcela == 'b1yr')
+gg4 = gg_core %>% filter(parcela == 'b3yr')
+
+ggplot(gg3, aes(date,value, col=index))+ 
+  geom_line(aes(group=index), size = 1)+
+  #geom_point()+
+  labs(fill= "Plot",x="Year",y="B1yr - Control (% Relative difference Core)")+
+  geom_hline(yintercept = 0, color = "gray", linetype = "dashed")+
+  
+  annotate("text", x = c(3.5, 5.5), y = 110, label = "Dry")+
+  
+  
+  annotate("text", x = 3, y = -123, label = "Fire experiment", colour = "red")+
+  annotate("rect", xmin = 1, xmax = 6, ymin = -129, ymax = -135, alpha = 0.9, fill = "red")+
+  annotate("text", x = c(1.5, 3.7, 5.5), y = -130, label = "xx", colour = "yellow")+
+  
+  annotate("text", x = 6.5, y = -123, label = "Restoration", colour = "darkblue")+
+  annotate("rect", xmin = 6, xmax = 7, ymin = -129, ymax = -135, alpha = 0.9, fill = "darkblue")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90))
+
+ggplot(gg4, aes(date,value, col=index))+ 
+  geom_line(aes(group=index), size = 1)+
+  #geom_point()+
+  labs(fill= "Plot",x="Year",y="B3yr - Control (% Relative difference Core)")+
+  geom_hline(yintercept = 0, color = "gray", linetype = "dashed")+
+  
+  annotate("text", x = c(3.5, 5.5), y = 110, label = "Dry")+
+  
+  
+  annotate("text", x = 3, y = -123, label = "Fire experiment", colour = "red")+
+  annotate("rect", xmin = 1, xmax = 6, ymin = -129, ymax = -135, alpha = 0.9, fill = "red")+
+  annotate("text", x = c(1.5, 3.7, 5.5), y = -130, label = "xx", colour = "yellow")+
+  
+  annotate("text", x = 6.5, y = -123, label = "Restoration", colour = "darkblue")+
+  annotate("rect", xmin = 6, xmax = 7, ymin = -129, ymax = -135, alpha = 0.9, fill = "darkblue")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90))
 
 
 
