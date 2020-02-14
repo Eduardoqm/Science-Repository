d0de20a9efb28b3492b0a1a1a453426a8c35aee4
Eduardoqm <eduardobio2009@gmail.com>
2020-02-13 22:20:48 -0300

    Update Liteira_process.R

--- Doutorado/Processos de bancos de dados/Area1 Plot Data Process/Liteira_process.R
+++ Doutorado/Processos de bancos de dados/Area1 Plot Data Process/Liteira_process.R
@@ -11,11 +11,52 @@ setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de 
 litt = read.csv("1_master_liteira_area_1_jun2019.csv", sep = ",", header = TRUE)
 
 #Part 1 - Organize data =====================================================================
+litt = litt[,c(1,8,10,13)]
+colnames(litt) = c("parcela", "dist", "data", "litt")
+litt$dist = as.character(litt$dist)
+litt$parcela = as.character(litt$parcela)
+
+#Add plot info
+litt$parcela[litt$parcela == "A"] <- c("controle")
+litt$parcela[litt$parcela == "B"] <- c("b3yr")
+litt$parcela[litt$parcela == "C"] <- c("b1yr")
+
+#Add info edge and core
+#By transect
+litt$dist[litt$dist == "Bo"] <- c("borda")
+litt$dist[litt$dist == "AA"] <- c("borda")
+litt$dist[litt$dist == "AB"] <- c("borda")
+litt$dist[litt$dist == "A"] <- c("borda")
+litt$dist[litt$dist == "B"] <- c("borda")
+litt$dist[litt$dist == "C"] <- c("borda")
+litt$dist[litt$dist == "D"] <- c("borda")
+litt$dist[litt$dist == "E"] <- c("borda")
+litt$dist[litt$dist == "F"] <- c("borda")
+litt$dist[litt$dist == "G"] <- c("nucleo")
+litt$dist[litt$dist == "H"] <- c("nucleo")
+litt$dist[litt$dist == "I"] <- c("nucleo")
+litt$dist[litt$dist == "J"] <- c("nucleo")
+litt$dist[litt$dist == "K"] <- c("nucleo")
+litt$dist[litt$dist == "L"] <- c("nucleo")
+litt$dist[litt$dist == "M"] <- c("nucleo")
+litt$dist[litt$dist == "N"] <- c("nucleo")
+litt$dist[litt$dist == "O"] <- c("nucleo")
+litt$dist[litt$dist == "P"] <- c("nucleo")
+litt$dist[litt$dist == "Q"] <- c("nucleo")
+litt$dist[litt$dist == "R"] <- c("nucleo")
+litt$dist[litt$dist == "S"] <- c("nucleo")
+litt$dist[litt$dist == "T"] <- c("nucleo")
+litt$dist[litt$dist == "U"] <- c("nucleo")
 
 
 #Part 2 - Calculate tons by hectare==========================================================
 #We know that it has 500 meters per transect and now we have the width of the transect. We will have to multiply to have the area in m2 and transform it into hectares.
-
+litt$hec = as.numeric(((60*80)/10000)/10000)
+litt$ton = as.numeric(litt$litt/1000000)
+litt = litt %>% 
+  na.omit() %>% 
+  group_by(parcela, dist, data) %>% 
+  summarise(litt = mean(litt))
 
 
 
