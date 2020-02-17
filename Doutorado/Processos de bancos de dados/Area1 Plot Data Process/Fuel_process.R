cb8ab5fb9dc395890d0daca34f89942f4c408e62
Eduardoqm <eduardobio2009@gmail.com>
2020-02-17 14:49:00 -0300

    Update Fuel_process.R

--- Doutorado/Processos de bancos de dados/Area1 Plot Data Process/Fuel_process.R
+++ Doutorado/Processos de bancos de dados/Area1 Plot Data Process/Fuel_process.R
@@ -6,68 +6,77 @@ library(tidyverse)
 library(reshape2)
 
 #Data Bank ==================================================================================
-setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Area1-plot/Tanguro Parcela")
+setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Area1-plot/Fogo")
 
-litt = read.csv("1_master_liteira_area_1_jun2019.csv", sep = ",", header = TRUE)
+fuel = read.csv("Combustivel_Brown_04_10_Area1.csv", sep = ",", header = TRUE)
 
 #Part 1 - Organize data =====================================================================
-litt = litt[,c(1,8,10,13)]
-colnames(litt) = c("parcela", "dist", "data", "litt")
-litt$dist = as.character(litt$dist)
-litt$parcela = as.character(litt$parcela)
+fuel = fuel %>% 
+  filter(measure == "prf") %>% 
+  mutate(fuel = NI06 + NI25 + NI76) 
+
+fuel = fuel[,c(1,2,7)]
+
+#Separete Lines and Transects
+fuel$linha <- as.numeric(str_extract(fuel$ponto, "[0-9]+"))
+fuel$dist <- as.character(str_extract(fuel$ponto, "[A-Z]+"))
+fuel = fuel[,c(-1)]
+colnames(fuel) = c("data", "fuel","linha", "dist")
 
 #Add plot info
-litt$parcela[litt$parcela == "A"] <- c("controle")
-litt$parcela[litt$parcela == "B"] <- c("b3yr")
-litt$parcela[litt$parcela == "C"] <- c("b1yr")
+for (x in 1:10) {
+  fuel$linha[fuel$linha == x] <- "controle"
+}
+
+for (x in 11:20) {
+  fuel$linha[fuel$linha == x] <- "b3yr"
+}
+
+for (x in 21:31) {
+  fuel$linha[fuel$linha == x] <- "b1yr"
+}
 
 #Add info edge and core
 #By transect
-litt$dist[litt$dist == "Bo"] <- c("borda")
-litt$dist[litt$dist == "AA"] <- c("borda")
-litt$dist[litt$dist == "AB"] <- c("borda")
-litt$dist[litt$dist == "A"] <- c("borda")
-litt$dist[litt$dist == "B"] <- c("borda")
-litt$dist[litt$dist == "C"] <- c("borda")
-litt$dist[litt$dist == "D"] <- c("borda")
-litt$dist[litt$dist == "E"] <- c("borda")
-litt$dist[litt$dist == "F"] <- c("borda")
-litt$dist[litt$dist == "G"] <- c("nucleo")
-litt$dist[litt$dist == "H"] <- c("nucleo")
-litt$dist[litt$dist == "I"] <- c("nucleo")
-litt$dist[litt$dist == "J"] <- c("nucleo")
-litt$dist[litt$dist == "K"] <- c("nucleo")
-litt$dist[litt$dist == "L"] <- c("nucleo")
-litt$dist[litt$dist == "M"] <- c("nucleo")
-litt$dist[litt$dist == "N"] <- c("nucleo")
-litt$dist[litt$dist == "O"] <- c("nucleo")
-litt$dist[litt$dist == "P"] <- c("nucleo")
-litt$dist[litt$dist == "Q"] <- c("nucleo")
-litt$dist[litt$dist == "R"] <- c("nucleo")
-litt$dist[litt$dist == "S"] <- c("nucleo")
-litt$dist[litt$dist == "T"] <- c("nucleo")
-litt$dist[litt$dist == "U"] <- c("nucleo")
+fuel$dist[fuel$dist == "BORDA"] <- c("borda")
+fuel$dist[fuel$dist == "CAPIM"] <- c("borda")
+fuel$dist[fuel$dist == "AA"] <- c("borda")
+fuel$dist[fuel$dist == "AB"] <- c("borda")
+fuel$dist[fuel$dist == "A"] <- c("borda")
+fuel$dist[fuel$dist == "B"] <- c("borda")
+fuel$dist[fuel$dist == "C"] <- c("borda")
+fuel$dist[fuel$dist == "D"] <- c("borda")
+fuel$dist[fuel$dist == "E"] <- c("borda")
+fuel$dist[fuel$dist == "F"] <- c("borda")
+fuel$dist[fuel$dist == "G"] <- c("nucleo")
+fuel$dist[fuel$dist == "H"] <- c("nucleo")
+fuel$dist[fuel$dist == "I"] <- c("nucleo")
+fuel$dist[fuel$dist == "J"] <- c("nucleo")
+fuel$dist[fuel$dist == "K"] <- c("nucleo")
+fuel$dist[fuel$dist == "L"] <- c("nucleo")
+fuel$dist[fuel$dist == "M"] <- c("nucleo")
+fuel$dist[fuel$dist == "N"] <- c("nucleo")
+fuel$dist[fuel$dist == "O"] <- c("nucleo")
+fuel$dist[fuel$dist == "P"] <- c("nucleo")
+fuel$dist[fuel$dist == "Q"] <- c("nucleo")
+fuel$dist[fuel$dist == "R"] <- c("nucleo")
+fuel$dist[fuel$dist == "S"] <- c("nucleo")
+fuel$dist[fuel$dist == "T"] <- c("nucleo")
+fuel$dist[fuel$dist == "U"] <- c("nucleo")
 
+#Summary data
+colnames(fuel) = c("data", "fuel","parcela", "dist")
 
-#Part 2 - Calculate tons by hectare==========================================================
-#We know that the basket is 60x80 cm in size, so we calculate its area and convert it from cm2 to hectares. The weight of the litter is just to convert grams to tons.
-litt$hec = as.numeric(((60*80)/10000)/10000)
-litt$ton = as.numeric(litt$litt/1000000)
-
-litt = litt %>% 
+fuel = fuel %>% 
   na.omit() %>% 
   group_by(parcela, dist, data) %>% 
-  summarise(ton = sum(ton), hec = sum(hec))
-
-#Convert in Ton/Hectares
-litt$lit_ton_hec = as.numeric(litt$ton/litt$hec)
-litt = litt[,c(1,2,3,6)]
+  summarise(fuel = sum(fuel))
 
 
-#Part 3 - Export data as CSV ================================================================
+#Part 2 - Export data as CSV ================================================================
 setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Deposito/Banco de Dados Tanguro/Dados para analise cap1")
 
-#write.table(litt, "Liteira_tang.csv", sep = ",")
+#write.table(fuel, "Fuel_tang.csv", sep = ",")
 
 
 
