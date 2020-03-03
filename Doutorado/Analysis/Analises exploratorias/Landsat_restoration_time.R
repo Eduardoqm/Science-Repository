9e1fc8750fcbff4001b39942c4ef0fb432775b8d
Eduardoqm <eduardobio2009@gmail.com>
2020-03-03 13:40:43 -0300

    Update Landsat_restoration_time.R

--- Doutorado/Analysis/Analises exploratorias/Landsat_restoration_time.R
+++ Doutorado/Analysis/Analises exploratorias/Landsat_restoration_time.R
@@ -32,22 +32,40 @@ land$data = as.character(land$data)
 
 #Boxplots ======================================
 #Restoration time
-land2 = melt(land)
-
-land2 = land2 %>% 
-  filter(data > 2012)
-
 eqm = c("#F9A602","#CF0E0E","#00AFBB") #Pallete colors(Orange and Blue)
 
-a = ggplot(land2, aes(data,value, fill=parcela))+ 
+land2 = melt(land)
+
+land3 = land2 %>% 
+  filter(data > 2012) %>% 
+  filter(variable %in% c('ndvi','evi','vig'))
+
+
+a = ggplot(land3, aes(data,value, fill=parcela))+ 
   geom_boxplot(outlier.alpha = 0.3)+
   #geom_violin()+
-  facet_wrap(~variable, scales="free") +
+  #facet_wrap(~variable, scales="free") +
+  facet_grid(variable ~ ., scales="free")+
   theme_minimal()+
   theme(axis.text.x = element_text(angle = 90))+
   theme(panel.border = element_rect(colour = "gray", fill=NA, size=0.5))
 
-land_time = ggpar(a, palette = eqm)
+a = ggpar(a, palette = eqm)
+
+
+land4 = land2 %>% 
+  filter(data > 2012) %>% 
+  filter(variable %in% c('ndwi','ndii','nbri'))
+
+b = ggplot(land4, aes(data,value, fill=parcela))+ 
+  geom_boxplot(outlier.alpha = 0.3)+
+  facet_grid(variable ~ ., scales="free")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90))+
+  theme(panel.border = element_rect(colour = "gray", fill=NA, size=0.5))
+
+b = ggpar(b, palette = eqm)
+
+land_time = ggarrange(a, b, common.legend = TRUE, legend="bottom", ncol = 2, nrow = 1)
+
 land_time
-
-
