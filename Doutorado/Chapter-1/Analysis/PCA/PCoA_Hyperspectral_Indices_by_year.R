44c486f98389db5828cafe112538d55e550fd517
Eduardoqm <eduardobio2009@gmail.com>
2020-12-04 11:29:08 -0300

    Update PCoA_Hyperspectral_Indices_by_year.R

--- Doutorado/Chapter-1/Analysis/PCA/PCoA_Hyperspectral_Indices_by_year.R
+++ Doutorado/Chapter-1/Analysis/PCA/PCoA_Hyperspectral_Indices_by_year.R
@@ -71,18 +71,22 @@ p2012 = getyear(2012)
 
 #PCoA Analysis
 #Create a distance data
-p2004b = p2004[,c(-1,-2)]
-p2004c = as.matrix(p2004b)
+p2010b = p2010[,c(-1,-2)]
+p2010c = log(p2010b + 1)
 
 library(ape)
 library(vegan)
-veg = vegdist(p2004c, "bray")
+veg = vegdist(p2010c, "bray")
 res = pcoa(veg)
 
-res$values
+view(res$values)
 biplot(res)
 
+pcoa2010 = as.data.frame(res$vectors[,c(1,2)])
+pcoa2010$treat = p2010$Parcela
 
+ggplot(pcoa2010, aes(x=Axis.1, y=Axis.2, col = treat))+
+  geom_point()
 
 
 
@@ -96,209 +100,3 @@ biplot(res)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-library(vegan)
-
-## criar uma matriz de dissimilaridade usando Bray-Curtis
-especies.bray<-vegdist(p2004c,"bray")
-
-## fazer a PCoA usando a função cmdscale
-PCoA<-cmdscale(especies.bray, k=1, eig=T) ## k=número de eixos; eig=T para ver a explicação
-PCoA1<-cmdscale(especies.bray, k=2, eig=T) ## k=número de eixos; eig=T para ver a explicação
-names(PCoA1)
-#biplot(PCoA1)
-PCoA.scores=(as.data.frame(cbind(PCoA1$points, PCoA1$eig)))
-PCoA.eig=(as.data.frame(cbind(PCoA$eig)))
-colnames(PCoA.scores)=c("Eixo1", "Eixo2", "Autovalor")## alterar caso queira salvar mais de dois eixos
-PCoA.expl=(as.data.frame(cbind(PCoA$GOF, PCoA1$GOF)))## salvar a explicação dos eixos
-colnames(PCoA.expl)=c("Eixo1", "Eixo 1 e 2")## alterar caso queira salvar mais de dois eixos
-# edit(PCoA.eig)
-
-## Salvar os resultados
-path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Capitulo1/PCoA"
-setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Capitulo1/PCoA")
-dir()
-
-write.csv(PCoA.scores, "trees.PCoA.scores.stream_field.csv")
-write.csv(PCoA.eig, "trees.PCoA.eigenvalue.stream_field.csv")
-write.csv(PCoA.expl, "trees.PCoA.explicação.stream_field.csv", na=" ", row.names=F)
-summary(PCoA1)
-
-
-####### Anosim testar significância da separação entre grupos
-###MOdelo
-#library(vegan)
-#data(dune) # matriz primária
-#data(dune.env) # matriz secundária
-#anosim<-anosim(dune, dune.env$Management, permutations = 999, distance = "bray")
-#summary(anosim)
-#plot(anosim)
-
-####### Anosim testar significância da separação entre grupos para forma de uso da terra
-library(vegan)
-anosim<-anosim(p2004c, p2004$Parcela, permutations = 999, distance = "bray")
-summary(anosim)
-plot(anosim)
-
-rm (list=ls())
-
-### Plotando resultados PCoA
-
-path="C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Capitulo1/PCoA"
-setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Doutorado/Capitulo1/PCoA")
-dir()
-
-fi=read.csv("trees.PCoA.scores.stream_field.csv",h=T)
-head(fi)
-names(fi)
-
-fi$Landuse=factor(fi$X,levels = c("Forest-Stream","Forest-Upland","Cropland-Stream","Cropland-Edge"))
-
-
-library(ggplot2)
-
-# scale_shape_manual
-# http://www.sthda.com/english/wiki/ggplot2-point-shapes
-### Ploot for trees
-plot=ggplot(data = fi,aes(Eixo1,Eixo2))+
-  geom_point(data = fi,aes(shape = Landuse,colour = Landuse),size=5,alpha=0.9)+
-  scale_shape_manual(values=c(16,17,15,9))+
-  # scale_color_manual(values = c('orange', 'darkgreen'))+
-  # scale_fill_manual(values =  c('orange', 'darkgreen'))+
-  stat_ellipse(data = fi, aes(colour = Landuse, linetype=Landuse), lwd=1.9)+
-  labs(x = "Axis 1 \n(16.6%; eigenvalue = 5.0)", y="Axis 2 \n(10.9%; eigenvalue = 3.3)")+
-  # annotate("text", x = c(-0.7), y = c(0.45), label = c("a"),size=10)+
-  # annotate("text", x = c(-0.5), y = c(0.25), label = c("Trees"),size=10)+
-  # theme_bw(base_size = 16)+
-  # scale_y_continuous(labels=function(x) format(x, big.mark = ".", decimal.mark = ",", scientific = FALSE))+
-  # scale_x_continuous(labels=function(x) format(x, big.mark = ".", decimal.mark = ",", scientific = FALSE))+
-  scale_color_manual(values = c("darkgreen","chartreuse1", "orange","saddlebrown"))+
-  scale_fill_manual(values = c("darkgreen","chartreuse1", "orange","saddlebrown"))+
-  theme_light()+
-  # theme_bw(base_size = 16)+
-  theme_minimal(base_size = 16)+
-  theme(legend.justification = c(0,0),legend.position = c(.1,.1),
-        legend.title=element_blank(),
-        axis.text = element_text(size = 20, colour = "black"),
-        axis.title = element_text(size = 25),
-        text = element_text(size = 20));plot
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-dist2004 = dist(scale(t(p2004b), center = T, scale = T), method = "euclidean")
-
-mds2004 = cmdscale(dist2004, eig = T, x.ret = T)
-mds.var.per = round(mds2004$eig/sum(mds2004$eig)*100, 1)
-
-#Make data better to plot
-mds_val = mds2004$points
-mds_data = data.frame(Sample = rownames(mds_val),
-                      X=mds_val[,1],
-                      Y=mds_val[,2])
-
-#Plot PCoA
-ggplot(mds_data, aes(x=X, y=Y, label=Sample))+
-  geom_text()+
-  xlab(paste("MDS1 - ", mds.var.per[1], "%", sep=""))+
-  ylab(paste("MDS2 - ", mds.var.per[2], "%", sep=""))
