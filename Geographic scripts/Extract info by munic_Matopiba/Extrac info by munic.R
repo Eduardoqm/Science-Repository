ff26272407e308624b7094c98ab22e8698ed7669
Eduardoqm <eduardobio2009@gmail.com>
2020-12-07 10:22:05 -0300

    Update Extrac info by munic.R

--- Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
+++ Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
@@ -9,3 +9,8 @@ setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Programas/R/Science-Reposito
 img = raster("spera_latlong_2016.tif")
 muni = readOGR("shape_2016.shp")
 
+#Create column to id munic in shape data
+muni@data$muni_id = c(1:1385)
+
+#Extracting data
+muni_ext = extract(img, muni, na.rm = T, df = T)
