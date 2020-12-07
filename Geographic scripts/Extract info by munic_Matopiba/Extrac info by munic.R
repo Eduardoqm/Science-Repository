7e65af549db140a7cd68f27fa357b3543294c32d
Eduardoqm <eduardobio2009@gmail.com>
2020-12-07 11:54:13 -0300

    Update Extrac info by munic.R

--- Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
+++ Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
@@ -18,6 +18,23 @@ muni2 = muni[1:139,]
 muni2@data$muni_id = c(1:length(muni2$MUNI))
 
 #Separete Spera data in binary raster do wich class
+cls1 = img #Soy single cropped
+cls1[cls1 != 1] = 0
+
+cls2 = img #Corn single cropped
+cls2[cls2 != 2] = 0; cls2[cls2 == 2] = 1
+
+cls4 = img #Cotton single cropped
+cls4[cls4 != 4] = 0; cls4[cls4 == 4] = 1
+
+cls5 = img #Soy/Cotton double crop rotation
+cls5[cls5 != 5] = 0; cls5[cls5 == 5] = 1
+
+cls6 = img #Soy/Corn double crop roation
+cls6[cls != 6] = 0; cls6[cls == 6] = 1
+
+
+
 
 
 
