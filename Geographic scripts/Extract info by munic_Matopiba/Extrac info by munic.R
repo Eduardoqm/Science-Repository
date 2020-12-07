9c1091e1f4443df5fc145b41a74223f574c33354
Eduardoqm <eduardobio2009@gmail.com>
2020-12-07 11:37:47 -0300

    Update Extrac info by munic.R

--- Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
+++ Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
@@ -13,9 +13,11 @@ muni = readOGR("PAM_2019.csv.shp")
 
 
 #Make tests with toy ========================================================================
+#Reduce polygons to Tocantins
 muni2 = muni[1:139,]
 muni2@data$muni_id = c(1:length(muni2$MUNI))
 
+#Separete Spera data in binary raster do wich class
 
 
 
