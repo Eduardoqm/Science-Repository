504c7391afe9fc62509abb620dda6ec7ef6c42b6
Eduardoqm <eduardobio2009@gmail.com>
2020-12-07 12:44:13 -0300

    Munic_shape rename

--- Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
+++ Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
@@ -8,8 +8,7 @@ library(tidyverse)
 setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Programas/R/Science-Repository/Geographic scripts/Extract info by munic_Matopiba")
 
 img = raster("spera_latlong_2016.tif")
-#muni = readOGR("shape_2016.shp")
-muni = readOGR("PAM_2019.csv.shp")
+muni = readOGR("Matopiba_munic.shp")
 
 
 #Create column to id munic in shape data ====================================================
