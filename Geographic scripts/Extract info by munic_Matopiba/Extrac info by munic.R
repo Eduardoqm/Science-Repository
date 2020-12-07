526b8a5ac4831641915ea2c02ef818515d1e68cb
Eduardoqm <eduardobio2009@gmail.com>
2020-12-07 11:17:32 -0300

    New munic vector

--- Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
+++ Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
@@ -2,15 +2,25 @@
 
 library(raster)
 library(rgdal)
+library(tidyverse)
 
 #Load data ========================================
 setwd("C:/Users/Eduardo Q Marques/Documents/My Jobs/Programas/R/Science-Repository/Geographic scripts/Extract info by munic_Matopiba")
 
 img = raster("spera_latlong_2016.tif")
-muni = readOGR("shape_2016.shp")
+#muni = readOGR("shape_2016.shp")
+muni = readOGR("PAM_2019.csv.shp")
+
+
+#Make tests with toy ========================================================================
+muni2 = muni[1:139,]
+muni2@data$muni_id = c(1:1385)
+
+
+
 
 #Create column to id munic in shape data
-muni@data$muni_id = c(1:1385)
+
 
 #Extracting data
 muni_ext = extract(img, muni, na.rm = T, df = T)
