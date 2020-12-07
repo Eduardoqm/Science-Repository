019f005a275b3a1a41b981f1b5d7ad80a852cf94
Eduardoqm <eduardobio2009@gmail.com>
2020-12-07 11:22:12 -0300

    Update Extrac info by munic.R

--- Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
+++ Geographic scripts/Extract info by munic_Matopiba/Extrac info by munic.R
@@ -14,7 +14,7 @@ muni = readOGR("PAM_2019.csv.shp")
 
 #Make tests with toy ========================================================================
 muni2 = muni[1:139,]
-muni2@data$muni_id = c(1:1385)
+muni2@data$muni_id = c(1:length(muni2$MUNI))
 
 
 
