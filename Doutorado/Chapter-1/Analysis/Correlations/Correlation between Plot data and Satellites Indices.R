be2ae47fc0f73bf1d213b9ade10f0fd25e7e649e
Eduardoqm <eduardobio2009@gmail.com>
2021-02-01 09:19:26 -0300

    Learn k-means cluster

--- Doutorado/Chapter-1/Analysis/Correlations/Correlation between Plot data and Satellites Indices.R
+++ Doutorado/Chapter-1/Analysis/Correlations/Correlation between Plot data and Satellites Indices.R
@@ -5,6 +5,7 @@
 library(tidyverse)
 library(reshape2)
 library(ggplot2)
+library(ggpubr)
 library(mgcv)
 library(visreg)
 library(extrafont)
@@ -23,32 +24,102 @@ df$year = substr(df$year, 1,4)
 df = df %>% 
   filter(year == c(2005:2017)) %>% 
   unite("id", c("treat", "year"), sep = "_")
+
 df2 = df[,c(4,5,6)]
+df2 = df2 %>%
+  group_by(id, index) %>% 
+  summarise(value = mean(value))
 
 lai = lai %>% 
   filter(year != 2012) %>% 
   unite("id", c("parcela", "year"), sep = "_")
+
 lai2 = lai[,c(3,4)]
+lai2 = lai2 %>% 
+  group_by(id) %>% 
+  summarise(lai = mean(lai))
+
 
 litt = litt %>% 
   filter(year == c(2005:2017)) %>% 
   filter(year != 2012) %>% 
   unite("id", c("parcela", "year"), sep = "_")
+
 litt2 = litt[,c(1,5)]
+litt2 = litt2 %>% 
+  group_by(id) %>% 
+  summarise(lit_ton_hec = mean(lit_ton_hec))
 
 #Join Data ---------------------------------------------------------
 df3 = full_join(df2, lai2, by = "id")
 df4 = full_join(df3, litt2, by = "id")
 
 #Plot correlation --------------------------------------------------
-ggplot(df4, aes(x=value, y=lai))+
-  geom_point()+
-  facet_wrap(~index)
+a = ggplot(df4, aes(x=value, y=lai))+
+  geom_point(size = 3, col = "red")+
+  geom_smooth(method="lm", se=F, col = "red")+ 
+  facet_grid(cols = vars(index), scales = "free")+
+  stat_cor(show.legend = F)+
+  theme_bw()+
+  labs(x = "", y = "LAI")+
+  theme(text = element_text(family = "Times New Roman", size = 14))
+
+b = ggplot(df4, aes(x=value, y=lit_ton_hec))+
+  geom_point(size = 3, col = "orange")+
+  geom_smooth(method="lm", se=F, col = "orange")+ 
+  facet_grid(cols = vars(index), scales = "free")+
+  stat_cor(show.legend = F)+
+  theme_bw()+
+  labs(x = "", y = "Liteira (T/ha)")+
+  theme(text = element_text(family = "Times New Roman", size = 14))
+
+
+ggarrange(a, b,
+          common.legend = TRUE,
+          legend="bottom",
+          ncol = 1, nrow = 2)
 
 
 
 
 
+
+df5 = df4
+df5$lai = (df5$lai)/10
+
+ggplot(df5)+
+  geom_point(aes(x=value, y=lai), size = 3, col = "red")+
+  geom_smooth(aes(x=value, y=lai), method="lm", se=F, col = "red")+ 
+  stat_cor(aes(x=value, y=lai),
+           show.legend = F, col = "red")+
+  
+  geom_point(aes(x=value, y=lit_ton_hec), size = 3, col = "orange")+
+  geom_smooth(aes(x=value, y=lit_ton_hec), method="lm", se=F, col = "orange")+
+  facet_wrap(~index, scales="free")+
+  stat_cor(aes(x=value, y=lit_ton_hec),
+           show.legend = F, label.y = 0.4, col = "orange")+
+  
+  theme_bw()+
+  labs(x = "", y = "")+
+  theme(text = element_text(family = "Times New Roman", size = 14))
+
+
+  
+  ggplot(df4, aes(x = value))+
+    geom_point(aes(y=lai), size = 3, col = "red")+
+    geom_smooth(aes(y=lai), method="lm", se=F, col = "red")+ 
+    stat_cor(aes(x=value, y=lai),
+             show.legend = F, col = "red")+
+    
+    geom_point(aes(y=lit_ton_hec), size = 3, col = "orange")+
+    geom_smooth(aes(y=lit_ton_hec), method="lm", se=F, col = "orange")+
+    stat_cor(aes(x=value, y=lit_ton_hec),
+             show.legend = F, label.y = 4, col = "orange")+
+    
+    facet_wrap(~index, scales="free")+
+    theme_bw()+
+    labs(x = "", y = "")
+  theme(text = element_text(family = "Times New Roman", size = 14))
 
 #-----------------------------------
 
