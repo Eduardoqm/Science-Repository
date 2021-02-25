0cce2a70655b0ec9b7356a8ff4127025ba365565
Eduardoqm <eduardobio2009@gmail.com>
2021-02-25 17:03:36 -0300

    Update Time Series of Area-1 field data.R

--- Doutorado/Chapter-1/Analysis/Time Seires/Time Series of Area-1 field data.R
+++ Doutorado/Chapter-1/Analysis/Time Seires/Time Series of Area-1 field data.R
@@ -16,29 +16,56 @@ setwd("C:/Users/Eduardo Q Marques/Documents/Research/Doutorado/Banco de Dados Ta
 
 lai = read.csv( "LAI_full_tang.csv", sep = ",")
 litt = read.csv( "Liteira_full_tang.csv", sep = ",")
-#biomass = read.csv( "xxxxxxx", sep = ",")
+bmas = read.csv( "Biomass_full_tang.csv", sep = ",")
 
 #Modify some elements -------------------------------------------------------------------
+#LAI
 lai$parcela = as.character(lai$parcela)
 lai$parcela[lai$parcela == "control"] <- c("Controle")
 lai$parcela[lai$parcela == "b3yr"] <- c("B3yr")
 lai$parcela[lai$parcela == "b1yr"] <- c("B1yr")
+lai = na.omit(lai)
+lai$year = as.character(lai$year)
+colnames(lai)[3:5] = c("LAI", "Ano", "Parcela")
 
+#Litterfall
 litt$parcela = as.character(litt$parcela)
 litt$parcela[litt$parcela == "control"] <- c("Controle")
 litt$parcela[litt$parcela == "b3yr"] <- c("B3yr")
 litt$parcela[litt$parcela == "b1yr"] <- c("B1yr")
+litt = na.omit(litt)
+litt$year = as.character(litt$year)
+colnames(litt)[1] = c("Parcela")
+
+#Biomass
+colnames(bmas) = c("Parcela", "dist", "Ano", "biomass")
+bmas$Ano = as.character(bmas$Ano)
 
 #Plot boxplots ---------------------------------------------------------------------------
 eqm = c("orange", "red", "blue") #My color palette
 
 #LAI
-ggplot(lai, aes(x=year, y=lai, color = parcela))+
-  geom_boxplot(aes(group = year))
+ggplot(lai, aes(x=Ano, y=LAI, fill = Parcela))+
+  geom_boxplot(alpha = 0.7)+
+  theme_bw()+
+  scale_fill_manual(values = eqm)+
+  theme(text = element_text(family = "Times New Roman", size = 14))
 
+#Litterfall
+ggplot(litt, aes(x=year, y=lit_ton_hec, fill = Parcela))+
+  geom_boxplot(alpha = 0.7)+
+  theme_bw()+
+  labs(y = "Liteira Ton/hec", x = "Ano")+
+  scale_fill_manual(values = eqm)+
+  theme(text = element_text(family = "Times New Roman", size = 14))
 
-
-
+#Biomass
+ggplot(bmas, aes(x=Ano, y=biomass, fill = Parcela))+
+  geom_boxplot(alpha = 0.7)+
+  theme_bw()+
+  labs(y = "Biomassa Ton/hec", x = "Ano")+
+  scale_fill_manual(values = eqm)+
+  theme(text = element_text(family = "Times New Roman", size = 14))
 
 
 
