4c2399582d81684a59146856b8aebdca052c3132
Eduardoqm <eduardobio2009@gmail.com>
2021-06-01 18:04:05 -0300

    Update Convert XY trees to coordinates.R

--- Doutorado/Chapter-2/Exploration Analysis/Convert XY trees to coordinates.R
+++ Doutorado/Chapter-2/Exploration Analysis/Convert XY trees to coordinates.R
@@ -42,8 +42,34 @@ df2 = df2[,c(-4,-6,-7)]
 
 #40 tem linha(X), nsdist(Y) e lodist
 
-#==============================================================================================
-
+#Transformation ===============================================================================
+#Part 1 - Transect to meter
+df2$transecto = as.character(df2$transecto)
+df2$trasc_m[df2$transecto == "A"] <- 0
+df2$trasc_m[df2$transecto == "AA"] <- 5
+df2$trasc_m[df2$transecto == "AB"] <- 25
+df2$trasc_m[df2$transecto == "B"] <- 50
+df2$trasc_m[df2$transecto == "C"] <- 100
+df2$trasc_m[df2$transecto == "D"] <- 150
+df2$trasc_m[df2$transecto == "E"] <- 200
+df2$trasc_m[df2$transecto == "F"] <- 250
+df2$trasc_m[df2$transecto == "G"] <- 300
+df2$trasc_m[df2$transecto == "H"] <- 350
+df2$trasc_m[df2$transecto == "I"] <- 400
+df2$trasc_m[df2$transecto == "J"] <- 450
+df2$trasc_m[df2$transecto == "K"] <- 500
+df2$trasc_m[df2$transecto == "L"] <- 550
+df2$trasc_m[df2$transecto == "M"] <- 600
+df2$trasc_m[df2$transecto == "N"] <- 650
+df2$trasc_m[df2$transecto == "O"] <- 700
+df2$trasc_m[df2$transecto == "P"] <- 750
+df2$trasc_m[df2$transecto == "Q"] <- 800
+df2$trasc_m[df2$transecto == "R"] <- 850
+df2$trasc_m[df2$transecto == "S"] <- 900
+df2$trasc_m[df2$transecto == "T"] <- 950
+df2$trasc_m[df2$transecto == "U"] <- 1000
+#df2$trasc_m[df2$transecto == "NA"] <- c("eqm")
+df2$trasc_m[is.na(df2$transecto)] <- c(df2$nsdist)
 
 
 
