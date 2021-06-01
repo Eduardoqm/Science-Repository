01a7fe794169f5c36ef829b9ca1ccdbfac644f18
Eduardoqm <eduardobio2009@gmail.com>
2021-06-01 14:33:14 -0300

    Create Convert XY trees to coordinates.R

---
+++ Doutorado/Chapter-2/Exploration Analysis/Convert XY trees to coordinates.R
@@ -0,0 +1,14 @@
+#Convert XY trees to coordinates
+#Blowdown Data (AREA-1)
+#Eduardo Q Marques 01-06-2021
+
+library(tidyverse)
+library(reshape2)
+library(ggplot2)
+library(fmsb)
+
+#Load data
+setwd('C:/Users/Eduardo Q Marques/Documents/Research/Doutorado/Banco de Dados Tanguro/Area1-plot/Campo vento')
+
+df = read.csv("blowdown_full_update_2021.csv", sep = ",")
+
