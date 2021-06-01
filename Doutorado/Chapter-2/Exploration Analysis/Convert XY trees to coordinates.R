cb51a331457cb1822aefa4ad125e2ea3f40c1d3c
Eduardoqm <eduardobio2009@gmail.com>
2021-06-01 14:39:46 -0300

    Update Convert XY trees to coordinates.R

--- Doutorado/Chapter-2/Exploration Analysis/Convert XY trees to coordinates.R
+++ Doutorado/Chapter-2/Exploration Analysis/Convert XY trees to coordinates.R
@@ -33,21 +33,16 @@ df = read.csv("blowdown_full_update_2021.csv", sep = ",")
 
 #Interative Map ===============================================================================
 library(leaflet)
+library(raster)
 
-eqm2 <- colorFactor(c("#003024","#fee08b","#d73027","#00ff00","#228c22"), domain = values(class11),
-                    na.color = "transparent")
 
 leaflet() %>% 
   addTiles() %>% 
-  addMiniMap(position = "bottomleft") %>%
-  addProviderTiles("Esri.WorldImagery") %>% 
-  addRasterImage(class11, colors = eqm2, opacity = 0.8) %>% 
-  addPolygons(data = area1, color = "#000000",
-              weight = 1, fillOpacity = 0,
-              highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                  bringToFront = TRUE),
-              popup = area1@data$Nome, label = area1@data$Nome) %>% 
-  addLegend(pal = eqm2, 
-            values = values(class11), 
-            title = "Classes",
-            opacity = 1)
+  #addMiniMap(position = "bottomleft") %>%
+  addProviderTiles("Esri.WorldImagery")
+  #addRasterImage(class11, colors = eqm2, opacity = 0.8) %>% 
+  #addPolygons(data = area1, color = "#000000",
+   #           weight = 1, fillOpacity = 0,
+    #          highlightOptions = highlightOptions(color = "white", weight = 2,
+     #                                             bringToFront = TRUE),
+      #        popup = area1@data$Nome, label = area1@data$Nome)
