84b783b592f426348710e420494de6fcef5c7d26
Eduardoqm <eduardobio2009@gmail.com>
2021-07-30 16:02:16 -0300

    Update Testes com Infer.R

--- Doutorado/Chapter-1/Analysis/LMs, GLMs, etc/Infer/Testes com Infer.R
+++ Doutorado/Chapter-1/Analysis/LMs, GLMs, etc/Infer/Testes com Infer.R
@@ -7,6 +7,7 @@ library(reshape2)
 library(ggplot2)
 library(ggridges)
 library(scales)
+library(infer)
 
 #Data ======================================
 setwd("C:/Users/Eduardo Q Marques/Documents/Research/Doutorado/Banco de Dados Tanguro/Dados para analise cap1")
@@ -30,9 +31,6 @@ ggplot(ndvi04, aes(x = value, y = treat, fill = treat)) +
   theme_minimal() +
   theme(panel.grid.minor = element_blank())
 
-
-library(infer)
-
 diff <- ndvi04 %>% 
   specify(value ~ treat) %>% 
   calculate(stat = "F")
@@ -51,19 +49,56 @@ null_distn %>%
   get_p_value(obs_stat = diff, direction = "greater")
 
 
-#2008 -------------------------------------------------------------------------------------
+#2005 ------------------------------------------------------------------------------------
+ndvi05 = ndvi %>% filter(year == 2005)
+
+hist(ndvi05$value)
+
+diff <- ndvi05 %>% 
+  specify(value ~ treat) %>% 
+  calculate(stat = "F")
+diff
+
+null_distn <- ndvi05 %>%
+  specify(value ~ treat) %>%
+  hypothesize(null = "independence") %>%
+  generate(reps = 1000, type = "permute") %>%
+  calculate(stat = "F")
+
+visualize(null_distn) +
+  shade_p_value(obs_stat = diff, direction = "greater")
+
+null_distn %>%
+  get_p_value(obs_stat = diff, direction = "greater")
+
+
+#2006 ------------------------------------------------------------------------------------
+ndvi06 = ndvi %>% filter(year == 2006)
+
+hist(ndvi06$value)
+
+diff <- ndvi06 %>% 
+  specify(value ~ treat) %>% 
+  calculate(stat = "F")
+diff
+
+null_distn <- ndvi06 %>%
+  specify(value ~ treat) %>%
+  hypothesize(null = "independence") %>%
+  generate(reps = 1000, type = "permute") %>%
+  calculate(stat = "F")
+
+visualize(null_distn) +
+  shade_p_value(obs_stat = diff, direction = "greater")
+
+null_distn %>%
+  get_p_value(obs_stat = diff, direction = "greater")
+
+
+#2008 ------------------------------------------------------------------------------------
 ndvi08 = ndvi %>% filter(year == 2008)
 
 hist(ndvi08$value)
-
-ggplot(ndvi08, aes(x = value, y = treat, fill = treat)) +
-  stat_density_ridges(quantile_lines = TRUE, quantiles = 2, scale = 3, color = "white") + 
-  scale_fill_manual(values = c("orange", "red", "blue"), guide = FALSE) + 
-  #scale_x_continuous(labels = percent_format(accuracy = 1)) +
-  labs(x = "NDVI", y = NULL) +
-  theme_minimal() +
-  theme(panel.grid.minor = element_blank())
-
 
 diff <- ndvi08 %>% 
   specify(value ~ treat) %>% 
@@ -81,6 +116,84 @@ visualize(null_distn) +
 
 null_distn %>%
   get_p_value(obs_stat = diff, direction = "greater")
+
+
+#2010 ------------------------------------------------------------------------------------
+ndvi10 = ndvi %>% filter(year == 2010)
+
+hist(ndvi10$value)
+
+diff <- ndvi10 %>% 
+  specify(value ~ treat) %>% 
+  calculate(stat = "F")
+diff
+
+null_distn <- ndvi10 %>%
+  specify(value ~ treat) %>%
+  hypothesize(null = "independence") %>%
+  generate(reps = 1000, type = "permute") %>%
+  calculate(stat = "F")
+
+visualize(null_distn) +
+  shade_p_value(obs_stat = diff, direction = "greater")
+
+null_distn %>%
+  get_p_value(obs_stat = diff, direction = "greater")
+
+
+#2011 ------------------------------------------------------------------------------------
+ndvi11 = ndvi %>% filter(year == 2011)
+
+hist(ndvi11$value)
+
+diff <- ndvi11 %>% 
+  specify(value ~ treat) %>% 
+  calculate(stat = "F")
+diff
+
+null_distn <- ndvi11 %>%
+  specify(value ~ treat) %>%
+  hypothesize(null = "independence") %>%
+  generate(reps = 1100, type = "permute") %>%
+  calculate(stat = "F")
+
+visualize(null_distn) +
+  shade_p_value(obs_stat = diff, direction = "greater")
+
+null_distn %>%
+  get_p_value(obs_stat = diff, direction = "greater")
+
+
+#2012 ------------------------------------------------------------------------------------
+ndvi12 = ndvi %>% filter(year == 2012)
+
+hist(ndvi12$value)
+
+diff <- ndvi12 %>% 
+  specify(value ~ treat) %>% 
+  calculate(stat = "F")
+diff
+
+null_distn <- ndvi12 %>%
+  specify(value ~ treat) %>%
+  hypothesize(null = "independence") %>%
+  generate(reps = 1200, type = "permute") %>%
+  calculate(stat = "F")
+
+visualize(null_distn) +
+  shade_p_value(obs_stat = diff, direction = "greater")
+
+null_distn %>%
+  get_p_value(obs_stat = diff, direction = "greater")
+
+
+
+
+
+
+
+
+
 
 
 
