7bbe77b1429f58d604cdb5e3cb7448c845cd9e45
Eduardoqm <eduardobio2009@gmail.com>
2021-09-21 10:34:55 -0300

    Update Correlation between Hyperion and Landsat.R

--- Doutorado/Chapter-1/Analysis/Analises exploratorias/Correlations/Correlation between Hyperion and Landsat.R
+++ Doutorado/Chapter-1/Analysis/Analises exploratorias/Correlations/Correlation between Hyperion and Landsat.R
@@ -23,12 +23,12 @@ land$index = as.character(land$index)
 land$treat = as.character(land$treat)
 
 #Modify elements of dataframe
-land$index[land$index == "evi2"] <- c("EVI")
-land$index[land$index == "ndvi"] <- c("NDVI")
-land$index[land$index == "ndii"] <- c("NDII")
-land$index[land$index == "grnd"] <- c("GRND")
-land$index[land$index == "nbr"] <- c("NBR")
-land$index[land$index == "nbr2"] <- c("NBR2")
+land$index[land$index == "evi2"] <- c("EVI_landsat")
+land$index[land$index == "ndvi"] <- c("NDVI_landsat")
+land$index[land$index == "ndii"] <- c("NDII_landsat")
+land$index[land$index == "grnd"] <- c("GRND_landsat")
+land$index[land$index == "nbr"] <- c("NBR_landsat")
+land$index[land$index == "nbr2"] <- c("NBR2_landsat")
 land$treat[land$treat == "control"] <- c("Controle")
 land$treat[land$treat == "b3yr"] <- c("B3yr")
 land$treat[land$treat == "b1yr"] <- c("B1yr")
@@ -73,7 +73,78 @@ hy$treat[hy$treat == "control"] <- c("Controle")
 hy$treat[hy$treat == "b3yr"] <- c("B3yr")
 hy$treat[hy$treat == "b1yr"] <- c("B1yr")
 
+#Testing overlap ============================================================
+hy_evi = hy %>% 
+  filter(index == "EVI") %>% 
+  filter(year == 2004)
+land_evi = land %>% 
+  filter(index == "EVI") %>% 
+  filter(year == 2004)
+
+
+ggplot()+
+  geom_point(data = hy_evi, aes(x = x, y = y), col = "red", alpha = 0.3, size = 5)+
+  geom_point(data = land_evi, aes(x = x, y = y), col = "blue", alpha = 0.3, size = 5)+
+  coord_fixed()+
+  theme_bw()
+
+#Summarize information ===========================================================
+land = land %>%
+  na.omit() %>% 
+  unite("id", c("treat", "year"), sep = "_") %>% 
+  group_by(id, index) %>% 
+  summarise(value = mean(value)) 
+
+hy = hy %>%
+  na.omit() %>% 
+  unite("id", c("treat", "year"), sep = "_") %>% 
+  group_by(id, index) %>% 
+  summarise(value = mean(value)) 
+
+#Transform inddices in columns ===================================================
+
+
+
+
+#Calculate correlation ===========================================================
+getcor = function(x,w){
+  z = hy %>% 
+    na.omit() %>% 
+    filter(index == x)
+    
+  k = land %>%
+    na.omit() %>% 
+    filter(index == w)
+  
+  print(cor(z$value, k$value), method = "pearson")
+}
+
+getcor("VARI","GRND")
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Ver: https://www.biostars.org/p/412100/
+
+
+
+
+
+
+
+
+
+
+
 
 
 
