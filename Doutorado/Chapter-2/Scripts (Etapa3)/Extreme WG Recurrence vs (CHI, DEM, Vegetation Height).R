050cb625a64ec20947db9365350b979a4e5a1b37
Eduardoqm <eduardobio2009@gmail.com>
2023-01-18 14:13:31 -0300

    Regression with quadractic factor

--- Doutorado/Chapter-2/Scripts (Etapa3)/Extreme WG Recurrence vs (CHI, DEM, Vegetation Height).R
+++ Doutorado/Chapter-2/Scripts (Etapa3)/Extreme WG Recurrence vs (CHI, DEM, Vegetation Height).R
@@ -27,57 +27,92 @@ colnames(gedi) = c("id", "Higth", "x", "y")
 bioma = read.csv("Biome_coord.csv", sep = ",")
 colnames(bioma) = c("x", "y", "Biome")
 
+opt_chi = read.csv("Optimal_ERA5_Block_1979_2020.csv", sep = ",")
+
+chi = opt_chi %>%
+  group_by(x, y) %>%
+  slice(which.max(CHI))
+chi = chi[,c(5,6,7)]
 
 #Join data frames -------------------------------------------------------------
 dem = dem %>% unite("xy", x:y)
 wg = wg %>% unite("xy", x:y)
 gedi = gedi %>% unite("xy", x:y)
 bioma = bioma %>% unite("xy", x:y)
+chi = chi %>% unite("xy", x:y)
 
 df = full_join(dem, wg, id = "xy")
 df = full_join(df, gedi, id = "xy")
+df = full_join(df, chi, id = "xy")
 df = full_join(df, bioma, id = "xy")
+
 
 df = df %>% na.omit()
 df = df[,c(-4)]
 
+df$Frequency = df$Recurrence/41
+
 pont = df %>% filter(xy == "-52.25_-13") #Area1 point
 
+#Regression with quadratic factor for Wind Frequency x DEM -------------------
+model = lm(Frequency~DEM+I(DEM^2), data = df)
+summary(model)
+
+df$predito=predict(model)
+
 #Correlation plots ------------------------------------------------------------
-a = ggplot(df, aes(x=DEM, y=Recurrence))+
-  geom_point(aes(col = Biome), size = 4, alpha = 0.7)+
-  geom_point(data = pont, aes(x=DEM, y=Recurrence), col = "red", alpha = 0.7,
+a = ggplot(df, aes(x=CHI, y=Frequency))+
+  geom_point(col = "purple", size = 4, alpha = 0.7)+
+  geom_point(data = pont, aes(x=CHI, y=Frequency), col = "red", alpha = 0.7,
              shape = 24, size = 3, stroke = 2)+
   stat_cor(show.legend = F)+
-  geom_smooth(col = "black")+
+  geom_smooth(col = "black", method = "lm")+
+  #scale_colour_manual(values = c("darkgreen", "darkorange"))+
+  labs(x="CHI Coefficient",
+       y="Frequency/year of Wind Gust (>10 m/s)",
+       title = "a) Extreme Wind (1979-2020) x Opitmal CHI")+
+  #theme_minimal()+
+  theme(legend.position = c(0.85, 0.8)); a
+
+b = ggplot(df, aes(x=DEM, y=Frequency))+
+  geom_point(aes(col = Biome), size = 4, alpha = 0.7)+
+  geom_point(data = pont, aes(x=DEM, y=Frequency), col = "red", alpha = 0.7,
+             shape = 24, size = 3, stroke = 2)+
+  stat_cor(show.legend = F)+
+  geom_smooth(aes(x=DEM, y=predito), col = "black")+
   scale_colour_manual(values = c("darkgreen", "darkorange"))+
   labs(x="Elevation Above Sea Level (m)",
-       y="Recurrence of Wind Gust (>10 m/s)",
-       title = "a) Extreme Wind (1979-2020) x Elevation Model")+
+       y="Frequency/year of Wind Gust (>10 m/s)",
+       title = "b) Extreme Wind (1979-2020) x Elevation Model")+
   #theme_minimal()+
-  theme(legend.position = c(0.85, 0.2)); a
+  theme(legend.position = c(0.85, 0.2)); b
 
 
-b = ggplot(df, aes(x=Recurrence, y=Higth))+
+c = ggplot(df, aes(x=Frequency, y=Higth))+
   geom_point(aes(col = Biome), size = 4, alpha = 0.7)+
-  geom_point(data = pont, aes(x=Recurrence, y=Higth), col = "red", alpha = 0.7,
+  geom_point(data = pont, aes(x=Frequency, y=Higth), col = "red", alpha = 0.7,
              shape = 24, size = 3, stroke = 2)+
-  stat_cor(show.legend = F, label.y.npc="top", label.x.npc = 0.4)+
-  facet_wrap(~Biome)+
+  stat_cor(show.legend = F, label.y.npc="top", label.x.npc = 0.7)+
+  stat_cor(aes(col = Biome), show.legend = F, label.y.npc = 0.85, label.x.npc = 0.7)+
+  #facet_wrap(~Biome)+
   scale_colour_manual(values = c("darkgreen", "darkorange"))+
   geom_smooth(method = "lm", col = "black")+
-  labs(x="Recurrence of Wind Gust (>10 m/s)",
+  labs(x="Frequency/year of Wind Gust (>10 m/s)",
        y="Vegetation Height (m)",
-       title = "b) Forest Height (2019) x Extreme Wind (1979-2020)")+
+       title = "c) Forest Height (2019) x Extreme Wind (1979-2020)")+
   #theme_minimal()+
-  theme(legend.position = "none"); b
+  theme(legend.position = c(0.09, 0.15)); c
 
 
-ggsave(filename = "Extreme Wind (1979-2020) x Elevation Model.png", plot = a,
+ggsave(filename = "Extreme Wind (1979-2020) x CHI.png", plot = a,
        path = "C:/Users/Eduardo Q Marques/Documents/Research/Doutorado/Capitulo2/Figuras/Paper_Figures",
        width = 16, height = 10, units = "cm", dpi = 300)
 
-ggsave(filename = "Forest Height x Extreme Wind (1979-2020).png", plot = b,
+ggsave(filename = "Extreme Wind (1979-2020) x Elevation Model.png", plot = b,
+       path = "C:/Users/Eduardo Q Marques/Documents/Research/Doutorado/Capitulo2/Figuras/Paper_Figures",
+       width = 16, height = 10, units = "cm", dpi = 300)
+
+ggsave(filename = "Forest Height x Extreme Wind (1979-2020).png", plot = c,
        path = "C:/Users/Eduardo Q Marques/Documents/Research/Doutorado/Capitulo2/Figuras/Paper_Figures",
        width = 16, height = 10, units = "cm", dpi = 300)
 
