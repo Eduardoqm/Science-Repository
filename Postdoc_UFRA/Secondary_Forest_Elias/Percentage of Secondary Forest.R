c37ec000e98da58fbb86fcebc206dee1834eec5a
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-02-25 16:54:31 -0300

    Update Percentage of Secondary Forest.R

--- Postdoc_UFRA/Secondary_Forest_Elias/Percentage of Secondary Forest.R
+++ Postdoc_UFRA/Secondary_Forest_Elias/Percentage of Secondary Forest.R
@@ -17,12 +17,32 @@ secf2 = secf
 secf2[secf2 > 0] = 1
 secf2[is.na(secf2)] = 0
 
+
+
+#Resample by not traditional way -----------------------------------------------
+secf_3 = resample(secf2, base, method = "sum")
+
+
+
+
+
+base2 <- as.polygons(base, fun=function(x){x>=0})
+
+#Counting
+secf3 = terra::extract(secf2, base2[1,], fun = count, df = F, na.rm = T)
+
+
+
+
+
+
+
 secf_3 = resample(secf2, base, method = "count")
 
 
 
 
-plot(secf)
+plot(secf2)
 plot(m_age, add = T)
 
 #writeRaster(m_age, "Perc_SecForest_1km.tif")
