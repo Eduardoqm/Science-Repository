4661e847544a6c249c00701ee73a9419cbc1f063
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-02-25 17:10:36 -0300

    Update Percentage of Secondary Forest.R

--- Postdoc_UFRA/Secondary_Forest_Elias/Percentage of Secondary Forest.R
+++ Postdoc_UFRA/Secondary_Forest_Elias/Percentage of Secondary Forest.R
@@ -18,7 +18,7 @@ secf2 = secf
 secf2[secf2 > 0] = 1
 secf2[is.na(secf2)] = 0
 
-#https://rdrr.io/cran/terra/man/resample.html
+#To understand resample -> https://rdrr.io/cran/terra/man/resample.html
 secf_3 = resample(secf2, base, method = "sum", threads = T)
 
 
