469ff0fe4f252997fe605c956017d90ca8e4373d
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-03-12 13:04:50 -0300

    Create Secondary Forest _30_perc.R

---
+++ Postdoc_UFRA/Secondary_Forest_Elias/Secondary Forest _30_perc.R
@@ -0,0 +1,83 @@
+#Secondary Forest > 30% (Elias Paper)
+
+#Eduardo Q Marques 12-03-2025
+
+library(terra)
+library(sf)
+
+#Load data ---------------------------------------------------------------------
+setwd("C:/Users/Eduardo/Documents/Analises_Elias/Rasters")
+dir()
+
+sec = rast("Perc_SecForest_1km.tif")
+pri = rast("Perc_PriForest_1km.tif")
+mage = rast("Mean_Forest_age_1km.tif")
+agri = rast("Perc_Agriculture_1km.tif")
+et = rast("ET_Amazonia_2023_1km.tif")
+lst = rast("LST_Amazonia_2023_1km.tif")
+amr = read_sf("C:/Users/Eduardo/Documents/Analises_Elias/Shapes")
+
+#Binary filtering > 30% of secondary forest ------------------------------------
+sec2 = sec
+sec2[sec2 < 30] = NA
+
+df = st_as_sf(as.points(sec2, na.rm = T))
+colnames(df)[1] = c("Perc_secforest")
+
+#Extracting others information -------------------------------------------------
+getv = function(z, x){
+  dfx = extract(x, z)
+  z = cbind(dfx[,2], z)
+}
+
+df = getv(df, pri); colnames(df)[1] = c("Perc_priforest")
+df = getv(df, agri); colnames(df)[1] = c("Perc_agriculture")
+df = getv(df, mage); colnames(df)[1] = c("Age_secforest")
+df = getv(df, et); colnames(df)[1] = c("ET")
+df = getv(df, lst); colnames(df)[1] = c("LST")
+
+sf_use_s2(FALSE)
+df = st_intersection(amr[1], df); colnames(df)[1] = c("Regions")
+df$Regions = substr(df$Regions, 1, 2)
+
+plot(df)
+
+#writeRaster(secf4, "Perc_SecForest_1km.tif")
+names(df)
+
+df2=df%>%
+  st_drop_geometry()%>%
+  dplyr::mutate(LST=ifelse(LST<24,NA,LST))
+
+library(ggplot2)
+
+ggplot(df2, aes(x = Age_secforest, y = LST))+
+  geom_point(aes(colour = Perc_agriculture),alpha=0.1)+
+  geom_smooth(method = 'lm')+
+  facet_wrap(Regions~.,scales = 'free')+
+  scale_color_gradient(low='darkgreen',high = 'red')
+
+
+ggplot(df2, aes(x = Age_secforest, y = LST))+
+  geom_point(aes(colour = Perc_secforest),alpha=0.5)+
+  geom_smooth(method = 'lm')+
+  facet_wrap(Regions~.,scales = 'free')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
