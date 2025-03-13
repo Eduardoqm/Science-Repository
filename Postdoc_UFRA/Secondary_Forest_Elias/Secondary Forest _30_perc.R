edf6385ef294548df0eeb7d69367bdcf55c9bbcb
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-03-13 09:24:10 -0300

    Update Secondary Forest _30_perc.R

--- Postdoc_UFRA/Secondary_Forest_Elias/Secondary Forest _30_perc.R
+++ Postdoc_UFRA/Secondary_Forest_Elias/Secondary Forest _30_perc.R
@@ -77,31 +77,40 @@ ggplot(df2, aes(x = Age_secforest, y = ET))+
 
 #Models ------------------------------------------------------------------------
 #LST Model
-m_lst=lmer(LST~Age_secforest*Regions+(1|Perc_agriculture)+(1|Perc_priforest),data=df2)
+m_lst=lmer(LST~Age_secforest*Regions+(1|Perc_agriculture)+(1|Perc_priforest),data=df2,
+           control=lmerControl(check.nobs.vs.nlev = "ignore",
+                               check.nobs.vs.rankZ = "ignore",
+                               check.nobs.vs.nRE="ignore"))
+
 summary(m_lst)
 MuMIn::r.squaredGLMM(m_lst)
 
 df2$pred_lst=predict(m_lst)
 
 ggplot(df2, aes(x = Age_secforest, y = LST))+
-  geom_point(aes(colour = Perc_agriculture),alpha=0.1)+
+  geom_point(aes(colour = Perc_agriculture))+
   geom_smooth(aes(x=Age_secforest, y=pred_lst))+
   facet_wrap(Regions~.,scales = 'free')+
-  scale_color_gradient(low='darkgreen',high = 'red')
+  scale_color_gradient(low='blue',high = 'yellow')+
+  theme_minimal()
 
 
 #ET Model
-m_et=lmer(ET~Age_secforest*Regions+(1|Perc_agriculture)+(1|Perc_priforest),data=df2)
+m_et=lmer(ET~Age_secforest*Regions+(1|Perc_agriculture)+(1|Perc_priforest),data=df2,
+          control=lmerControl(check.nobs.vs.nlev = "ignore",
+                              check.nobs.vs.rankZ = "ignore",
+                              check.nobs.vs.nRE="ignore"))
 summary(m_et)
 MuMIn::r.squaredGLMM(m_et)
 
 df2$pred_et=predict(m_et)
 
 ggplot(df2, aes(x = Age_secforest, y = ET))+
-  geom_point(aes(colour = Perc_agriculture),alpha=0.1)+
+  geom_point(aes(colour = Perc_agriculture))+
   geom_smooth(aes(x=Age_secforest, y=pred_et))+
   facet_wrap(Regions~.,scales = 'free')+
-  scale_color_gradient(low='darkgreen',high = 'red')
+  scale_color_gradient(low='blue',high = 'yellow')+
+  theme_minimal()
 
 
 
