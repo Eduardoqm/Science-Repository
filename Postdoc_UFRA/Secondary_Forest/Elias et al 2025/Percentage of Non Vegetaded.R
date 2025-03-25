9bf338471dae887a358953f048ade20eaf66075d
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-03-25 14:39:41 -0300

    Create Percentage of Non Vegetaded.R

---
+++ Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Percentage of Non Vegetaded.R
@@ -0,0 +1,33 @@
+#Percentage of Water (Elias Paper)
+
+#Eduardo Q Marques 25-03-2025
+
+library(terra)
+
+#Load data ---------------------------------------------------------------------
+setwd("G:/Meu Drive/Postdoc_UFRA/Papers/Serra (Elias et al)/Analises_Elias/Rasters")
+dir()
+
+base = rast("ET_Amazonia_2023_1km.tif")
+mb = rast("MB2023_l1_30m.tiff")
+
+#Calculating percentage of Water class -----------------------------------------
+#Make binary to count
+mb2 = mb
+mb2[mb2 != 26] = 0 # Class Water for L1 (26)
+mb2
+
+ttl = mb #Total of pixels to extract fraction
+ttl[ttl != 1] = 1
+
+
+#To understand resample -> https://rdrr.io/cran/terra/man/resample.html
+mb3 = resample(mb2, base, method = "sum")
+ttl2 = resample(ttl, base, method = "sum")
+
+mb4 = ((mb3/ttl2)*100)/26
+
+plot(mb)
+plot(mb4, add = T)
+
+writeRaster(mb4, "Perc_Water_1km.tif")
