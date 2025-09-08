9a22f9168767491faf54c18cbbe573340ec9ef54
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-09-08 17:01:00 -0300

    Update Ecosist_Services_ET_vs_AGB.R

--- Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Focal Analysis/Ecosist_Services_ET_vs_AGB.R
+++ Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Focal Analysis/Ecosist_Services_ET_vs_AGB.R
@@ -92,7 +92,7 @@ rm(esa2, et_delta_pri)
 colnames(resf) = c("agb", "delta_et", "sf_perc")
 resf2 = resf %>% na.omit()
 rm(resf)
-resf2$cond = "Dry"
+resf2$cond = "Dry Season"
 
 write.csv(resf2, "ET_AGB_Dry_full.csv", row.names = F)
 rm(resf2)
@@ -121,7 +121,7 @@ rm(esa2, et_delta_pri)
 colnames(resf) = c("agb", "delta_et", "sf_perc")
 resf2 = resf %>% na.omit()
 rm(resf)
-resf2$cond = "Rainy"
+resf2$cond = "Rainy Season"
 
 write.csv(resf2, "ET_AGB_Rainy_full.csv", row.names = F)
 
