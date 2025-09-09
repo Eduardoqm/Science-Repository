26c4946eeae4d34258f80b667f2c52e0f2e0dca9
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-09-09 15:45:28 -0300

    Update Ecosist_Services_ET_vs_Pasture.R

--- Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Focal Analysis/Ecosist_Services_ET_vs_Pasture.R
+++ Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Focal Analysis/Ecosist_Services_ET_vs_Pasture.R
@@ -19,105 +19,93 @@ plot(et_year)
 plot(et_dry)
 plot(et_wet)
 
-#Primary Forest
-fr_pri = rast("Forest_70m.tif" )
-plot(fr_pri)
-
-#grass Biomass
-grass = rast("Pasture_70m.tif")
+#Pasture
+grass=rast("Pasture_70m.tif")
 plot(grass)
+
+#Secondary Forest
+sf=rast("MB_Forest_age_70m.tif")
+plot(sf)
 
 #Percentage of Secondary Forest
 sf_perc = rast("Perc_SecForest_70m.tif")
 plot(sf_perc)
 
 #Proccess before Focal ---------------------------------------------------------
-#sf_perc = ifel(sf_perc == 0,NA, sf_perc)
-#plot(sf_perc)
+sf_perc = ifel(sf_perc == 0,NA, sf_perc)
+plot(sf_perc)
 
 #Focal Function -------------------------------------------------------------
 setwd("G:\\Meu Drive\\Postdoc_UFRA\\Papers\\Serrapilheira (Elias et al)\\Analises_Elias\\Dados\\")
 start.time <- Sys.time()
 
+gc()
 #ET Annual
 #Calculating et for Primary Forest
-et_pri=ifel(is.na(fr_pri),NA,et_year)
-#plot(et_pri)
+et_pri=ifel(is.na(grass),NA,et_year)
 
 #Use focal to calculate difference in Secondary and Primary (Delta)
 et_f <- focal(et_pri, w=21, median, na.rm=TRUE, na.policy="only")
-#plot(et_f)
 rm(et_pri)
 
+gc()
 et_delta_pri=et_year-et_f
 rm(et_year, et_f)
 plot(et_delta_pri)
 
-grass2 = ifel(is.na(et_delta_pri),NA, grass)
-plot(grass2)
-
-resf=as.data.frame(c(grass2,et_delta_pri))
+gc()
+resf=as.data.frame(c(sf,et_delta_pri, sf_perc))
 rm(grass2, et_delta_pri)
-colnames(resf) = c("pasture", "delta_et")
+colnames(resf) = c("sf_age", "delta_et", "sf_perc")
 resf2 = resf %>% na.omit()
-rm(resf)
 resf2$cond = "Annual Season"
 
 write.csv(resf2, "ET_Pasture_Annual_full.csv", row.names = F)
-rm(resf2)
+
 
 #ET Dry Season
+gc()
 #Calculating et for Primary Forest
-et_pri=ifel(is.na(fr_pri),NA,et_dry)
-#plot(et_pri)
+et_pri=ifel(is.na(grass),NA,et_dry)
 
 #Use focal to calculate difference in Secondary and Primary (Delta)
 et_f <- focal(et_pri, w=21, median, na.rm=TRUE, na.policy="only")
-#plot(et_f)
 rm(et_pri)
 
+gc()
 et_delta_pri=et_dry-et_f
-rm(et_dry, et_f)
+rm(et_year, et_f)
 plot(et_delta_pri)
 
-grass2 = ifel(is.na(et_delta_pri),NA, grass)
-plot(grass2)
-
 gc()
-resf=as.data.frame(c(grass2,et_delta_pri))
+resf=as.data.frame(c(sf,et_delta_pri, sf_perc))
 rm(grass2, et_delta_pri)
-colnames(resf) = c("pasture", "delta_et")
+colnames(resf) = c("sf_age", "delta_et", "sf_perc")
 resf2 = resf %>% na.omit()
-rm(resf)
 resf2$cond = "Dry Season"
 
 write.csv(resf2, "ET_Pasture_Dry_full.csv", row.names = F)
-rm(resf2)
+
 
 #ET Rainy Season
 gc()
 #Calculating et for Primary Forest
-et_pri=ifel(is.na(fr_pri),NA,et_wet)
-#plot(et_pri)
+et_pri=ifel(is.na(grass),NA,et_wet)
 
 #Use focal to calculate difference in Secondary and Primary (Delta)
 et_f <- focal(et_pri, w=21, median, na.rm=TRUE, na.policy="only")
-#plot(et_f)
 rm(et_pri)
 
+gc()
 et_delta_pri=et_wet-et_f
-rm(et_wet, et_f)
+rm(et_year, et_f)
 plot(et_delta_pri)
 
-grass2 = ifel(is.na(et_delta_pri),NA, grass)
-plot(grass2)
-
 gc()
-resf=as.data.frame(c(grass2,et_delta_pri))
+resf=as.data.frame(c(sf,et_delta_pri, sf_perc))
 rm(grass2, et_delta_pri)
-colnames(resf) = c("pasture", "delta_et")
+colnames(resf) = c("sf_age", "delta_et", "sf_perc")
 resf2 = resf %>% na.omit()
-rm(resf)
 resf2$cond = "Rainy Season"
 
 write.csv(resf2, "ET_Pasture_Rainy_full.csv", row.names = F)
