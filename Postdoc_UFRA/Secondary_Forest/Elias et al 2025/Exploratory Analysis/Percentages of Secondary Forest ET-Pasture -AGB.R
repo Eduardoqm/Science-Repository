de0dd76a6e7819d92876fb78489f475154417ede
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-09-12 18:08:20 -0300

    Update Percentages of Secondary Forest ET-Pasture -AGB.R

--- Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Exploratory Analysis/Percentages of Secondary Forest ET-Pasture -AGB.R
+++ Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Exploratory Analysis/Percentages of Secondary Forest ET-Pasture -AGB.R
@@ -49,7 +49,7 @@ setwd("G:\\Meu Drive\\Postdoc_UFRA\\Papers\\Serrapilheira (Elias et al)\\Analise
 gg5 = ggplot(df5, aes(x=agb, y=et, col = cond))+
   geom_point(size = 3)+
   stat_smooth()+
-  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (mm/year)",
+  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (%)",
        col = "Condition", title = "Secondary Forest vs. Pasture - Purity >5%")+
   scale_color_manual(values = c("#66bd63", "#fc8d59", "#67a9cf"))+
   theme_minimal(); gg5
@@ -61,7 +61,7 @@ ggsave(plot = gg5, "Delta_ET_Past_AGB_5_perc.png", dpi = 300,
 gg30 = ggplot(df30, aes(x=agb, y=et, col = cond))+
   geom_point(size = 3)+
   stat_smooth()+
-  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (mm/year)",
+  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (%)",
        col = "Condition", title = "Secondary Forest vs. Pasture - Purity >30%")+
   scale_color_manual(values = c("#66bd63", "#fc8d59", "#67a9cf"))+
   theme_minimal(); gg30
@@ -73,7 +73,7 @@ ggsave(plot = gg30, "Delta_ET_Past_AGB_30_perc.png", dpi = 300,
 gg50 = ggplot(df50, aes(x=agb, y=et, col = cond))+
   geom_point(size = 3)+
   stat_smooth()+
-  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (mm/year)",
+  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (%)",
        col = "Condition", title = "Secondary Forest vs. Pasture - Purity >50%")+
   scale_color_manual(values = c("#66bd63", "#fc8d59", "#67a9cf"))+
   theme_minimal(); gg50
@@ -84,7 +84,7 @@ ggsave(plot = gg50, "Delta_ET_Past_AGB_50_perc.png", dpi = 300,
 gg70 = ggplot(df70, aes(x=agb, y=et, col = cond))+
   geom_point(size = 3)+
   stat_smooth()+
-  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (mm/year)",
+  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (%)",
        col = "Condition", title = "Secondary Forest vs. Pasture - Purity >70%")+
   scale_color_manual(values = c("#66bd63", "#fc8d59", "#67a9cf"))+
   theme_minimal(); gg70
@@ -96,7 +96,7 @@ ggsave(plot = gg70, "Delta_ET_Past_AGB_70_perc.png", dpi = 300,
 gg100 = ggplot(df100, aes(x=agb, y=et, col = cond))+
   geom_point(size = 3)+
   stat_smooth()+
-  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (mm/year)",
+  labs(x="Aboveground Biomass (Mg/ha)",y="Δ Evapotranspiration (%)",
        col = "Condition", title = "Secondary Forest vs. Pasture - Purity >100%")+
   scale_color_manual(values = c("#66bd63", "#fc8d59", "#67a9cf"))+
   theme_minimal(); gg100
