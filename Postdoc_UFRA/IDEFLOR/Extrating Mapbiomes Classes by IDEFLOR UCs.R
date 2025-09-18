1b1908adeeb3f61e06097de1f6a00273e772adf7
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-09-18 14:24:15 -0300

    Update Extrating Mapbiomes Classes by IDEFLOR UCs.R

--- Postdoc_UFRA/IDEFLOR/Extrating Mapbiomes Classes by IDEFLOR UCs.R
+++ Postdoc_UFRA/IDEFLOR/Extrating Mapbiomes Classes by IDEFLOR UCs.R
@@ -1,6 +1,6 @@
 #Extrating Mapbiomes Classes by IDEFLOR UCs
 
-#Eduar Q Marques 18-09-2025
+#Eduardo Q Marques 18-09-2025
 
 library(terra)
 library(sf)
@@ -28,3 +28,43 @@ for (z in 1:18) {
 
 idclas$area_ha = (idclas$npixel*900)/10000
 idclas$UC = "All"
+
+#By UCs
+mbi = mask(crop(mb, ucs[1,2]), ucs[1,2])
+idclas_a = idclas
+idclas_b = idclas
+
+for (z in 1:18) {
+  print(idclas[z,1])
+  idclas_a[z,2] <- freq(mb2 == idclas_a[z,1])[2,3]
+  idclas_a$area_ha = (idclas_a$npixel*900)/10000
+}
+idclas_a$UC = ucs$nome_[1]
+idclas_a = rbind(idclas_a, idclas_b)
+
+
+
+for (i in 2:3) {
+  ucs$nome_[i]
+  mbi = mask(crop(mb, ucs[i,2]), ucs[i,2])
+  for (z in 1:18) {
+    print(idclas[z,1])
+    idclas_b[z,2] <- freq(mb2 == idclas[z,1])[2,3]
+    idclas_b$area_ha = (idclas$npixel*900)/10000
+    idclas$UC = ucs$nome_[i]
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
