30434f2430ee8f6997d2b2155b4f6eca56e4759d
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-09-18 15:12:47 -0300

    Update Extrating Mapbiomes Classes by IDEFLOR UCs.R

--- Postdoc_UFRA/IDEFLOR/Extrating Mapbiomes Classes by IDEFLOR UCs.R
+++ Postdoc_UFRA/IDEFLOR/Extrating Mapbiomes Classes by IDEFLOR UCs.R
@@ -16,45 +16,32 @@ mb2 = mask(crop(mb, ucs), ucs)
 plot(mb2)
 
 #Dataframe to input classes
-idclas = unique(mb2)
-idclas$npixel = 0
+df = freq(mb2)
 
-#freq(mb2 == 0)[2,3]
-
-for (z in 1:18) {
-  print(idclas[z,1])
-  idclas[z,2] <- freq(mb2 == idclas[z,1])[2,3]
-}
-
-idclas$area_ha = (idclas$npixel*900)/10000
-idclas$UC = "All"
+df$area_ha = (df$count*900)/10000
+df$UC = "All"
 
 #By UCs
 mbi = mask(crop(mb, ucs[1,2]), ucs[1,2])
-idclas_a = idclas
-idclas_b = idclas
 
-for (z in 1:18) {
-  print(idclas[z,1])
-  idclas_a[z,2] <- freq(mb2 == idclas_a[z,1])[2,3]
-  idclas_a$area_ha = (idclas_a$npixel*900)/10000
-}
-idclas_a$UC = ucs$nome_[1]
-idclas_a = rbind(idclas_a, idclas_b)
+df2 = freq(mbi)
+df2$area_ha = (df2$count*900)/10000
+df2$UC = ucs$nome_[1]
 
 
-
-for (i in 2:3) {
-  ucs$nome_[i]
+for (i in 1:29) {
+  print(ucs$nome_[i])
   mbi = mask(crop(mb, ucs[i,2]), ucs[i,2])
-  for (z in 1:18) {
-    print(idclas[z,1])
-    idclas_b[z,2] <- freq(mb2 == idclas[z,1])[2,3]
-    idclas_b$area_ha = (idclas$npixel*900)/10000
-    idclas$UC = ucs$nome_[i]
-  }
+  plot(mbi)
+  
+  df2 = freq(mbi)
+  df2$area_ha = (df2$count*900)/10000
+  df2$UC = ucs$nome_[i]
+  
+  df = rbind(df, df2)
 }
 
+#Input classes names
 
 
 
