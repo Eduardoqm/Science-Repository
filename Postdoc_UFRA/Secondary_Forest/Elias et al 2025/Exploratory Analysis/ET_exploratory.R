67c44ca59c7b216550bb8adbebe364a2e6118ad0
Eduardo Q Marques <eduardobio2009@gmail.com>
2025-09-26 14:37:24 -0300

    Update ET_exploratory.R

--- Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Exploratory Analysis/ET_exploratory.R
+++ Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Exploratory Analysis/ET_exploratory.R
@@ -1,6 +1,6 @@
-#LST Exploratory Analysis
+#ET Exploratory Analysis
 
-#Eduardo Q Marques 15-09-2025
+#Eduardo Q Marques 26-09-2025
 
 library(ggplot2)
 library(tidyverse)
@@ -10,64 +10,64 @@ setwd("G:/Meu Drive/Postdoc_UFRA/Papers/Serrapilheira (Elias et al)/Analises_Eli
 dir()
 
 #Secondary Forest - Primary Forest
-lst_a = read.csv("LST_SecFor_Age_Annual_full.csv")
-lst_b = read.csv("LST_SecFor_Age_Dry_full.csv")
-lst_c = read.csv("LST_SecFor_Age_Rainy_full.csv")
+et_a = read.csv("ET_SecFor_Age_Annual_full.csv")
+et_b = read.csv("ET_SecFor_Age_Dry_full.csv")
+et_c = read.csv("ET_SecFor_Age_Rainy_full.csv")
 
-lst_pri = rbind(lst_a, lst_b, lst_c)
-lst_pri$test = "a) Secondary - Primary Forest"
+et_pri = rbind(et_a, et_b, et_c)
+et_pri$test = "a) Secondary - Primary Forest"
 
 #Secondary Forest - Pasture
-lst_a = read.csv("LST_Pasture_Annual_full.csv")
-lst_b = read.csv("LST_Pasture_Dry_full.csv")
-lst_c = read.csv("LST_Pasture_Rainy_full.csv")
+et_a = read.csv("ET_Pasture_Annual_full.csv")
+et_b = read.csv("ET_Pasture_Dry_full.csv")
+et_c = read.csv("ET_Pasture_Rainy_full.csv")
 
-lst_past = rbind(lst_a, lst_b, lst_c)
-lst_past$test = "b) Secondary Forest - Pasture"
+et_past = rbind(et_a, et_b, et_c)
+et_past$test = "b) Secondary Forest - Pasture"
 
-lst_sf = rbind(lst_pri, lst_past)
+et_sf = rbind(et_pri, et_past)
 
 #Secondary Forest - Primary Forest x AGB
-lst_a = read.csv("LST_AGB_Annual_full.csv")
-lst_b = read.csv("LST_AGB_Dry_full.csv")
-lst_c = read.csv("LST_AGB_Rainy_full.csv")
+et_a = read.csv("ET_AGB_Annual_full.csv")
+et_b = read.csv("ET_AGB_Dry_full.csv")
+et_c = read.csv("ET_AGB_Rainy_full.csv")
 
-lst_pri_agb = rbind(lst_a, lst_b, lst_c)
-lst_pri_agb$test = "c) Secondary - Primary Forest x AGB"
+et_pri_agb = rbind(et_a, et_b, et_c)
+et_pri_agb$test = "c) Secondary - Primary Forest x AGB"
 
 #Secondary Forest - Pasture x AGB
-lst_a = read.csv("LST_AGB_Past_Annual_full.csv")
-lst_b = read.csv("LST_AGB_Past_Dry_full.csv")
-lst_c = read.csv("LST_AGB_Past_Rainy_full.csv")
+et_a = read.csv("ET_AGB_Past_Annual_full.csv")
+et_b = read.csv("ET_AGB_Past_Dry_full.csv")
+et_c = read.csv("ET_AGB_Past_Rainy_full.csv")
 
-lst_past_agb = rbind(lst_a, lst_b, lst_c)
-lst_past_agb$test = "d) Secondary Forest - Pasture x AGB"
+et_past_agb = rbind(et_a, et_b, et_c)
+et_past_agb$test = "d) Secondary Forest - Pasture x AGB"
 
-lst_agb = rbind(lst_pri_agb, lst_past_agb)
+et_agb = rbind(et_pri_agb, et_past_agb)
 
-lst_agb$grupo = cut(lst_agb$agb, breaks = seq(0,10000, by = 10), labels = F)
+et_agb$grupo = cut(et_agb$agb, breaks = seq(0,10000, by = 10), labels = F)
 
 #Filtering data ----------------------------------------------------------------
-lst_sf2 = lst_sf %>% 
+et_sf2 = et_sf %>% 
   filter(sf_perc >= 70)%>% 
   mutate(sf_age=round(sf_age,0))%>%
   group_by(test, sf_age, cond)%>%
-  summarise(lst=mean(delta_lst,na.rm=T))
+  summarise(et=mean(delta_et,na.rm=T))
 
-lst_agb2 = lst_agb %>% 
+et_agb2 = et_agb %>% 
   filter(sf_perc >= 70)%>% 
   filter(agb < 401) %>% 
   mutate(agb=round(agb,0))%>%
   group_by(test, cond, grupo)%>%
-  summarise(agb = mean(agb),lst=mean(delta_lst,na.rm=T))
+  summarise(agb = mean(agb),et=mean(delta_et,na.rm=T))
 
 #Plotting Results --------------------------------------------------------------
 setwd("G:/Meu Drive/Postdoc_UFRA/Papers/Serrapilheira (Elias et al)/Analises_Elias/Figures")
 
-plt1 = ggplot(lst_sf2, aes(x=sf_age, y=lst, col = cond))+
+plt1 = ggplot(et_sf2, aes(x=sf_age, y=et, col = cond))+
   geom_point(size = 3)+
   stat_smooth()+
-  labs(x="Secondary forest age (year)",y="Δ LST (C°)",
+  labs(x="Secondary forest age (year)",y="Δ et (C°)",
        col = "Condition")+
   scale_color_manual(values = c("#66bd63", "#fc8d59", "#67a9cf"))+
   facet_wrap(~test, scales = "free", strip.position = "top")+
@@ -76,14 +76,14 @@ plt1 = ggplot(lst_sf2, aes(x=sf_age, y=lst, col = cond))+
         strip.text = element_text(size=13),
         legend.position = "none"); plt1
 
-ggsave(plot = plt1, "Delta_LST_Results.png", dpi = 300,
+ggsave(plot = plt1, "Delta_et_Results.png", dpi = 300,
        height = 10, width = 30, units = "cm")
 
 
-plt2 = ggplot(lst_agb2, aes(x=agb, y=lst, col = cond))+
+plt2 = ggplot(et_agb2, aes(x=agb, y=et, col = cond))+
   geom_point(size = 3)+
   stat_smooth()+
-  labs(x="Aboveground Biomass (Mg/ha)",y="Δ LST (C°)",
+  labs(x="Aboveground Biomass (Mg/ha)",y="Δ et (C°)",
        col = "Condition")+
   scale_color_manual(values = c("#66bd63", "#fc8d59", "#67a9cf"))+
   facet_wrap(~test, scales = "free")+
@@ -92,7 +92,7 @@ plt2 = ggplot(lst_agb2, aes(x=agb, y=lst, col = cond))+
         strip.text = element_text(size=13),
         legend.position = c(0.9, 0.5)); plt2
 
-ggsave(plot = plt2, "Delta_LST_AGB_Results.png", dpi = 300,
+ggsave(plot = plt2, "Delta_et_AGB_Results.png", dpi = 300,
        height = 10, width = 30, units = "cm")
 
 
