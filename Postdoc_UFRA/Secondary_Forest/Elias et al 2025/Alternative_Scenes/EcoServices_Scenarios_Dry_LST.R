4604ea7e426eb9fa9d8b4914b538dc6ea8ad1d2f
Eduardo Q Marques <eduardobio2009@gmail.com>
2026-02-12 16:17:19 -0300

    Update EcoServices_Scenarios_Dry_LST.R

--- Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Alternative_Scenes/EcoServices_Scenarios_Dry_LST.R
+++ Postdoc_UFRA/Secondary_Forest/Elias et al 2025/Alternative_Scenes/EcoServices_Scenarios_Dry_LST.R
@@ -13,6 +13,7 @@ scf = rast("MB_Forest_age_70m.tif")
 past = rast("Pasture_70m.tif")
 prf = rast("Forest_70m.tif")
 dlt_lst_p = rast("Pasture_Delta_LST_Dry.tif")
+df_delta = read.csv("G:/Meu Drive/Dados_Elias_paper/Delta_Data_Frame/LST_Pasture_age_FULL.csv")
 
 plot(lst_cur)
 plot(dlt_lst_p)
@@ -38,6 +39,16 @@ mean(values(lst_cur),na.rm=T) #33.76452
 
 
 #Scenery 2 -> Converting Pasture in a 38 year old Forest -----------------------
+#Scenery 2 and 3-> Converting Pasture in sf and persisting to 38 years ---------
+
+#Input min, mean and maximun
+
+
+
+
+
+
+
 #mean(3.27, 3.51, 3.30) #delta in 38 years old from the three years (2022 to 2024)
 sf_lmar = -3.36 #Limiar value
 
@@ -52,6 +63,42 @@ mean(values(scn_sf2),na.rm=T) #32.84293
 #Result
 32.84 - 33.76 #-0.92
 #Converting Pasture to 38 year old SF, the air temperature decrease 0.9°C.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #Scenery 3 -> Forest until 10 years holding 20 years without deforestation -----
@@ -70,25 +117,55 @@ df2 = df %>%
   group_by(Age2) %>% 
   summarise(Temp_C = mean(Temp_C))
 
+df2$Temp_C20 = 0
+
+#Until 19 years old 
+for (z in 1:19) {
+  k = z+19
+  df2[z,3] = df2[z,2]-df2[k,2]
+}
+
+#20 years more
+for (z in 20:38) {
+  df2[z,3] = df2[z,2]-df2[38,2]
+}
+
+write.csv(df2, "SF_LST_Mean_diff_20years.csv", row.names = F)
+
+#Applying differences in SF LST
+diff_list <- list(df2$Temp_C20)
+scf2 = round(scf, digits = 0)
+
+#scf_lst2 = ifel(scf2 == 1, scf_lst-diff_list[[1]][1], scf_lst)
+
+for (z in 1:37) {
+  print(z)
+  scf_lst2 = ifel(scf2 == z, scf_lst-diff_list[[1]][z], scf_lst)
+}
+
+scn_sf3 = ifel(is.na(scf_lst2), lst_cur, scf_lst2)
+
+mean(values(scn_sf3),na.rm=T) #33.7643
+
+33.7643 - 33.76452 #-0.00022
+
+#Persistence of young secondary forests for 20 years without deforestation decrease the air temperature in 0.00022°C.
 
 
 
+mean(values(scf_lst2), na.rm = T)
+mean(values(scf_lst), na.rm = T)
 
 
+ggplot(df2, aes(x = Age2))+
+  geom_point(aes(y = Temp_C), col = "blue")+
+  geom_point(aes(y = Temp_C-Temp_C20))
+  #geom_smooth()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+ggplot(df2, aes(x = Age2, y = Temp_C20))+
+  geom_point()+
+  geom_smooth()
 
 
 
