55afb0b09d6d4be1d6b5e27078ced644d3f3aad9
Ludmila Rattis <eduardobio2009@gmail.com>
2026-03-18 15:54:34 -0300

    Update H3.R

--- Postdoc_GCBC/Paper_VPD_ERA5/H3.R
+++ Postdoc_GCBC/Paper_VPD_ERA5/H3.R
@@ -1,10 +1,12 @@
-#H3
+# H3 - The increase in VPD has been more pronounced
+# in southern Amazon than in central Amazon over
+# the past five decades.
 
 #Eduardo Q Marques 10-03-2026
 
 library(tidyverse)
 
-setwd("G:/My Drive/Research/PosDoc_GCBC/Analises/Toy_H3")
+setwd("G:/My Drive/Research/PosDoc_GCBC/Analises/H3")
 dir()
 
 df = read_csv("ERA5_CP_VPD_since1975.csv")
@@ -28,7 +30,7 @@ for (z in rain_months) {
 
 df$year = as.numeric(df$year); head(df)
 
-#Itensity
+#Itensity ----------------------------------------------------------------------
 #model1 <- lm(Intesidade ~ Ano * Season + Ano * Região)
 df2 = df %>% 
   group_by(Region, year, cond) %>% 
@@ -45,17 +47,23 @@ ggplot(df2, aes(x=year, y=VPD_int, col = cond))+
   facet_wrap(~factor(Region, c("NW","NE","SW","SE")), scales = "free")+
   theme_bw()
 
-#Flamability duration
+#Flamability duration ----------------------------------------------------------
 #model1 <- lm(Duração ~ Ano * Season + Ano * Região)
 df$contagem = 1
 df3 = df %>% 
   group_by(Region, year, cond) %>% 
   filter(VPD >= 0.75) %>% 
-  summarise(VPD_time = sum(contagem))
+  summarise(VPD_time = (sum(contagem)/30)/24)
 head(df3)
 
 model2 <- lm(VPD_time ~ year * cond + year * Region, data = df3)
 summary(model2)
+
+#Residual standard error: 261.3 on 398 degrees of freedom
+#Multiple R-squared:  0.8401,	Adjusted R-squared:  0.8365 
+#F-statistic: 232.4 on 9 and 398 DF,  p-value: < 2.2e-16
+
+
 
 ggplot(df3, aes(x=year, y=VPD_time, col = cond))+
   geom_point()+
