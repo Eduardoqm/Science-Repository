8010378634b0e4d2478240e2924cb2b64c73b57f
Ludmila Rattis <eduardobio2009@gmail.com>
2026-05-16 10:20:10 -0300

    Update Kestrel_and_hobo_master.R

--- Postdoc_GCBC/Paper_VPD_ERA5/Kestrel_and_hobo_master.R
+++ Postdoc_GCBC/Paper_VPD_ERA5/Kestrel_and_hobo_master.R
@@ -21,10 +21,10 @@ sdp_road = read.csv("D2_-_3104596-ESTRADA_7_de_mai._de_2026___10_20_00_AM.csv",
 
 
 #Processing dataframe ----------------------------------------------------------
-prcs_kestrel = function(bd, age, sample){
+prcs_kestrel = function(bd, age, sample, sensor){
   bd = bd[-1,c(1:3)]
   colnames(bd) = c("Date", "Temp_C", "RH")
-  bd$age = age; bd$sample = sample
+  bd$age = age; bd$sample = sample; bd$sensor = sensor
   
   bd$Date = as.POSIXct(bd$Date, format = "%Y-%m-%d %H:%M")
   bd$Temp_C = as.numeric(gsub(",", ".", bd$Temp_C))
@@ -32,14 +32,31 @@ prcs_kestrel = function(bd, age, sample){
   return(bd)
 }
 
-sdp_road2 = prcs_kestrel(sdp_road, 18, "Estrada SEDAP")
-ufra2 = prcs_kestrel(ufra, 20, "UFRA")
-duquinha2 = prcs_kestrel(duquinha, 32, "Reserva S.Geraldo")
+sdp_road2 = prcs_kestrel(sdp_road, 18, "Estrada SEDAP", "kestrel_d2")
+ufra2 = prcs_kestrel(ufra, 20, "UFRA", "kestrel_d2")
+duquinha2 = prcs_kestrel(duquinha, 32, "Reserva S.Geraldo", "kestrel_d2")
 duquinha2 = duquinha2 %>% filter(Date > "2026-04-26 00:00:00")
 
 
 #Mastering ---------------------------------------------------------------------
 master = rbind(sdp_road2, ufra2, duquinha2)
+
+#Calibration by HOBO LM slope --------------------------------------------------
+#Intercept+(Slope*Kestrel)
+master$Temp_C <- 4.462359 + (0.814337 * master$Temp_C)
+master$RH <- 41.648026 + (0.572683 * master$RH)
+
+#Calculating VPD ---------------------------------------------------------------
+vpd <- function(temp, UR) {
+  es <- 0.6108 * exp((17.27 * temp) / (temp + 237.3)) #pressao de saturacao
+  ea <- es * (UR / 100)                               #pressao real
+  VPD <- es - ea                                      #deficit de pressao de vapor
+  return(VPD)
+}
+
+master$VPD = vpd(master$Temp_C, master$RH)
+
+#Exporting Master --------------------------------------------------------------
 
 #Exploration Graphs-------------------------------------------------------------
 ggplot(master, aes(x = Date, y = Temp_C, col = sample))+
@@ -49,3 +66,11 @@ ggplot(master, aes(x = Date, y = Temp_C, col = sample))+
 ggplot(master, aes(x = Date, y = RH, col = sample))+
   geom_point(size = 1, alpha = 0.5)+
   geom_smooth()
+
+ggplot(master, aes(x = Date, y = VPD, col = sample))+
+  geom_point(size = 1, alpha = 0.5)+
+  geom_smooth()
+
+ggplot(master, aes(x = sample, y = VPD, col = sample))+
+  geom_boxplot()
+
