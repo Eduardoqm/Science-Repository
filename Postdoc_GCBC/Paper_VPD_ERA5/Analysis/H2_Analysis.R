e56664d0af059a36e82829bb88973d6d424b2a23
Ludmila Rattis <eduardobio2009@gmail.com>
2026-08-25 13:29:45 -0300

    Update H2_Analysis.R

--- Postdoc_GCBC/Paper_VPD_ERA5/Analysis/H2_Analysis.R
+++ Postdoc_GCBC/Paper_VPD_ERA5/Analysis/H2_Analysis.R
@@ -11,16 +11,54 @@ library(tidyverse)
 setwd("G:/My Drive/Research/PosDoc_GCBC/Dados e Analises/H2")
 dir()
 
+df_full = read_csv("Hours_VPD75_Age_full.csv")
+df = read_csv("Hours_VPD75_Age.csv")
 
 
 #Graphics ----------------------------------------------------------------------
-ggplot(df4, aes(x=Age, y=Hours))+
+ggplot(df_full, aes(x=Age, y=Hours))+
+  #geom_point()+
+  geom_smooth(method = "lm")
+
+ggplot(df_full, aes(x=Age, y=Hours, col=Month))+
+  #geom_point()+
+  geom_smooth(method = "lm")+
+  facet_wrap(~Month, scale = "free")
+
+
+ggplot(df, aes(x=Age, y=Hours))+
   geom_point()+
   geom_smooth(method = "lm")
 
-ggplot(df4, aes(x=Age, y=Hours, col=Month))+
+ggplot(df, aes(x=Age, y=Hours, col=Month))+
   geom_point()+
   geom_smooth(method = "lm")+
   facet_wrap(~Month, scale = "free")
+
+
+df2 = df |> 
+  na.omit() |> 
+  group_by(Age, Month) |> 
+  summarise(Hours = median(Hours),
+            n = n())
+
+ggplot(df2, aes(x=Age, y=Hours))+
+  geom_point()+
+  geom_smooth(method = "lm")
+
+ggplot(df2, aes(x=Age, y=Hours, col=Month))+
+  geom_point()+
+  geom_smooth()+
+  facet_wrap(~Month, scale = "free")
+
+
+#Logaritmar os dados para rodar o modelo...
+
+
+
+
+
+
+
 
 
