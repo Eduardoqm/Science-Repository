49f108a93e4debd37dcacb5d2cb68e65bf34a343
Eduardo Q Marques <eduardobio2009@gmail.com>
2026-08-26 15:53:23 -0300

    Create H2_Proccess to extract info_2025.R

---
+++ Postdoc_GCBC/Paper_VPD_ERA5/Analysis/H2_Proccess to extract info_2025.R
@@ -0,0 +1,70 @@
+#Paper VPD ERA5 Amazon - Proccess to extract info - 2025
+# H2 - Along the successional gradient, young secondary forests
+#remain exposed for longer periods to microclimatic conditions
+#that favor fire spread throughout the dry season.
+
+#Eduardo Q Marques 26-08-2026
+
+library(terra)
+library(tidyverse)
+
+#Load data ---------------------------------------------------------------------
+#Secondary Forest
+#Workstation
+scf24 = rast("/home/leaf/Documentos/Paper_VPD_Marques_et_al/Rasters_H2/MB_Forest_age_2024B.tif")
+plot(scf24)
+
+scf23 = scf24 - 1
+scf23 = ifel(scf23 < 1, NA, scf23)
+plot(scf23)
+
+#VPD >= 075 kPa hours by month
+#Workstation
+list_rst = list.files("/home/leaf/Documentos/Paper_VPD_Marques_et_al/Rasters_H2/VPD_month_2023", full.names = T); list_rst
+
+h_vpd = rast(list_rst)
+plot(h_vpd)
+
+#Extracting by random points -----------------------------------------------
+smp <- spatSample(scf23, size = 1000000, method = "random",
+                  as.points = TRUE, na.rm = TRUE)
+
+plot(scf23)
+plot(smp, add = T)
+
+df = as.data.frame(smp)
+
+vpd = terra::extract(h_vpd, smp)
+
+df2 = cbind(vpd, df)
+
+colnames(df2) = c("id", "Jan", "Fev", "Mar", "April", "May", "June",
+                  "July", "Aug", "Set", "Oct", "Nov", "Dec", "Age")
+
+df3 = df2 |> 
+  pivot_longer(
+    cols = c(Jan, Fev, Mar, April, May, June, July, Aug, Set, Oct, Nov, Dec), 
+    names_to = "Month", 
+    values_to = "Hours")
+
+write.csv(df3, "Hours_VPD75_Age_full_2023.csv", row.names = F)
+
+df4 = df3 |> 
+  na.omit() |> 
+  group_by(Age, Month) |> 
+  summarise(Hours = mean(Hours),
+            n = n())
+
+write.csv(df4, "Hours_VPD75_Age_2023.csv", row.names = F)
+
+
+ggplot(df4, aes(x=Age, y=Hours))+
+  geom_point()+
+  geom_smooth(method = "lm")
+
+ggplot(df4, aes(x=Age, y=Hours, col=Month))+
+  geom_point()+
+  geom_smooth(method = "lm")+
+  facet_wrap(~Month, scale = "free")
+
+
